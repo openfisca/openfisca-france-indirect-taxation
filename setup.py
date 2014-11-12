b1dc1d9693727b92ecf7a9b0ce8ba2466c2f48a0
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-11-12 19:20:46 +0100

    Setting up

---
+++ setup.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""France specific indirect taxation model for OpenFisca -- a versatile microsimulation free software"""
+
+
+from setuptools import setup, find_packages
+
+
+classifiers = """\
+Development Status :: 2 - Pre-Alpha
+License :: OSI Approved :: GNU Affero General Public License v3
+Operating System :: POSIX
+Programming Language :: Python
+Topic :: Scientific/Engineering :: Information Analysis
+"""
+
+doc_lines = __doc__.split('\n')
+
+
+setup(
+    name = 'OpenFisca-France-Indirect-Taxation',
+    version = '0.4dev',
+
+    author = 'OpenFisca Team',
+    author_email = 'contact@openfisca.fr',
+    classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
+    description = doc_lines[0],
+    keywords = 'benefit france indirect microsimulation social tax',
+    license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
+    long_description = '\n'.join(doc_lines[2:]),
+    url = 'https://github.com/openfisca/openfisca-france-indirect-taxation',
+
+    data_files = [
+        ('share/locale/fr/LC_MESSAGES', ['openfisca_france_indirect_taxation/i18n/fr/LC_MESSAGES/openfisca-france-indirect-taxation.mo']),
+        ],
+    extras_require = dict(
+        tests = [
+            'pandas >= 0.13',
+            ],
+        ),
+    install_requires = [
+        'Babel >= 0.9.4',
+        'Biryani1[datetimeconv] >= 0.9dev',
+        'numpy',
+        'OpenFisca-Core >= 0.4dev',
+        'scipy >= 0.12',
+        ],
+    message_extractors = {'openfisca_france_indirect_taxation': [
+        ('**.py', 'python', None),
+        ]},
+    packages = find_packages(),
+    test_suite = 'nose.collector',
+    zip_safe = False,
+    )
