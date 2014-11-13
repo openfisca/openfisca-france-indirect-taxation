a84e441c5122d4146210ab241bb2fb06f2943143
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-11-13 12:29:55 +0100

    Embryonic and non functioning (WIP)

---
+++ openfisca_france_indirect_taxation/__init__.py
@@ -0,0 +1,324 @@
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import collections
+import datetime
+import itertools
+import os
+
+import numpy as np
+from numpy.core.defchararray import startswith
+
+from openfisca_core import conv
+from openfisca_core.columns import AgeCol, BoolCol, FloatCol, IntCol, reference_input_variable
+from openfisca_core.formulas import (alternative_function, AlternativeFormulaColumn, dated_function, DatedFormulaColumn,
+    EntityToPersonColumn, PersonToEntityColumn, make_reference_formula_decorator, SimpleFormulaColumn)
+from openfisca_core.scenarios import AbstractScenario
+from openfisca_core.taxbenefitsystems import AbstractTaxBenefitSystem
+
+
+COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
+CURRENCY = u"€"
+
+
+from .entities import entity_class_by_symbol, Menages, Individus
+
+
+# Scenarios
+
+
+class Scenario(AbstractScenario):
+    def init_single_entity(self, autres = None, axes = None, conjoint = None, enfants = None,
+                           menage = None, period = None, personne_de_reference = None):
+        if enfants is None:
+            enfants = []
+        if autres is None:
+            autres = []
+        assert personne_de_reference is not None
+        menage = menage.copy() if menage is not None else {}
+        individus = []
+        for index, individu in enumerate([personne_de_reference, conjoint] + (enfants or []) + (autres or [])):
+            if individu is None:
+                continue
+            id = individu.get('id')
+            if id is None:
+                individu = individu.copy()
+                individu['id'] = id = 'ind{}'.format(index)
+            individus.append(individu)
+            if index == 0:
+                menage['personne_de_reference'] = id
+            elif index == 1:
+                menage['conjoint'] = id
+            else:
+                menage.setdefault('enfants', []).append(id)
+        conv.check(self.make_json_or_python_to_attributes())(dict(
+            axes = axes,
+            period = period,
+            test_case = dict(
+                menages = [menage],
+                individus = individus,
+                ),
+            ))
+        return self
+
+    def make_json_or_python_to_test_case(self, period = None, repair = False):
+        assert period is not None
+
+        def json_or_python_to_test_case(value, state = None):
+            if value is None:
+                return value, None
+            if state is None:
+                state = conv.default_state
+
+            column_by_name = self.tax_benefit_system.column_by_name
+
+            # First validation and conversion step
+            test_case, error = conv.pipe(
+                conv.test_isinstance(dict),
+                conv.struct(
+                    dict(
+                        menages = conv.pipe(
+                            conv.condition(
+                                conv.test_isinstance(list),
+                                conv.pipe(
+                                    conv.uniform_sequence(
+                                        conv.test_isinstance(dict),
+                                        drop_none_items = True,
+                                        ),
+                                    conv.function(lambda values: collections.OrderedDict(
+                                        (value.pop('id', index), value)
+                                        for index, value in enumerate(values)
+                                        )),
+                                    ),
+                                ),
+                            conv.test_isinstance(dict),
+                            conv.uniform_mapping(
+                                conv.pipe(
+                                    conv.test_isinstance((basestring, int)),
+                                    conv.not_none,
+                                    ),
+                                conv.pipe(
+                                    conv.test_isinstance(dict),
+                                    conv.struct(
+                                        dict(itertools.chain(
+                                            dict(
+                                                autres = conv.pipe(
+                                                    conv.test_isinstance(list),
+                                                    conv.uniform_sequence(
+                                                        conv.test_isinstance((basestring, int)),
+                                                        drop_none_items = True,
+                                                        ),
+                                                    conv.default([]),
+                                                    ),
+                                                conjoint = conv.pipe(
+#                                                    conv.test_isinstance(dict),
+#                                                    conv.uniform_sequence(
+#                                                        conv.test_isinstance((basestring, int)),
+#                                                        drop_none_items = True,
+#                                                        ),
+                                                    conv.default(None),
+                                                    ),
+                                                enfants = conv.pipe(
+                                                    conv.test_isinstance(list),
+                                                    conv.uniform_sequence(
+                                                        conv.test_isinstance((basestring, int)),
+                                                        drop_none_items = True,
+                                                        ),
+                                                    conv.default([]),
+                                                    ),
+#                                                personne_de_reference = conv.pipe(
+#                                                    conv.test_isinstance(dict),
+#                                                    conv.uniform_sequence(
+#                                                        conv.test_isinstance((basestring, int)),
+#                                                        drop_none_items = True,
+#                                                        ),
+#                                                    conv.default(None),
+#                                                    ),
+                                                ).iteritems(),
+                                            (
+                                                (column.name, column.json_to_python)
+                                                for column in column_by_name.itervalues()
+                                                if column.entity == 'men'
+                                                ),
+                                            )),
+                                        drop_none_values = True,
+                                        ),
+                                    ),
+                                drop_none_values = True,
+                                ),
+                            conv.default({}),
+                            ),
+                        individus = conv.pipe(
+                            conv.condition(
+                                conv.test_isinstance(list),
+                                conv.pipe(
+                                    conv.uniform_sequence(
+                                        conv.test_isinstance(dict),
+                                        drop_none_items = True,
+                                        ),
+                                    conv.function(lambda values: collections.OrderedDict(
+                                        (value.pop('id', index), value)
+                                        for index, value in enumerate(values)
+                                        )),
+                                    ),
+                                ),
+                            conv.test_isinstance(dict),
+#                            conv.uniform_mapping(
+#                                conv.pipe(
+#                                    conv.test_isinstance((basestring, int)),
+#                                    conv.not_none,
+#                                    ),
+#                                conv.pipe(
+#                                    conv.test_isinstance(dict),
+#                                    conv.struct(
+#                                        dict(
+#                                            (column.name, column.json_to_python)
+#                                            for column in column_by_name.itervalues()
+#                                            if column.entity == 'ind' and column.name not in (
+#                                                'idmen', 'quimen')
+#                                            ),
+#                                        drop_none_values = True,
+#                                        ),
+#                                    ),
+#                                drop_none_values = True,
+#                                ),
+                            conv.empty_to_none,
+                            conv.not_none,
+                            ),
+                        ),
+                    ),
+                )(value, state = state)
+            if error is not None:
+                return test_case, error
+
+            # Second validation step
+            menages_individus_id = list(test_case['individus'].iterkeys())
+            test_case, error = conv.struct(
+                dict(
+                    menages = conv.uniform_mapping(
+                        conv.noop,
+                        conv.struct(
+                            dict(
+                                autres = conv.uniform_sequence(conv.test_in_pop(menages_individus_id)),
+                                conjoint = conv.uniform_sequence(conv.test_in_pop(menages_individus_id)),
+                                enfants = conv.uniform_sequence(conv.test_in_pop(menages_individus_id)),
+                                personne_de_reference = conv.uniform_sequence(conv.test_in_pop(menages_individus_id)),
+                                ),
+                            default = conv.noop,
+                            ),
+                        ),
+                    ),
+                default = conv.noop,
+                )(test_case, state = state)
+
+            remaining_individus_id = set(menages_individus_id)
+            if remaining_individus_id:
+                if error is None:
+                    error = {}
+                for individu_id in remaining_individus_id:
+                    error.setdefault('individus', {})[individu_id] = state._(u"Individual is missing from {}").format(
+                        state._(u' & ').join(
+                            word
+                            for word in [
+                                u'menages' if individu_id in menages_individus_id else None,
+                                ]
+                            if word is not None
+                            ))
+            if error is not None:
+                return test_case, error
+
+            return test_case, error
+
+        return json_or_python_to_test_case
+
+
+# TaxBenefitSystems
+
+
+def init_country():
+    class TaxBenefitSystem(AbstractTaxBenefitSystem):
+        entity_class_by_key_plural = {
+            entity_class.key_plural: entity_class
+            for entity_class in entity_class_by_symbol.itervalues()
+            }
+
+    # Define class attributes after class declaration to avoid "name is not defined" exceptions.
+    TaxBenefitSystem.Scenario = Scenario
+
+    return TaxBenefitSystem
+
+
+# Input variables
+
+
+reference_input_variable(
+    column = IntCol,
+    entity_class = Individus,
+    label = "Date de naissance",
+    name = 'birth',
+    )
+
+reference_input_variable(
+    column = IntCol,
+    entity_class = Individus,
+    is_permanent = True,
+    label = u"Identifiant du ménage",
+    name = 'id_menage',
+    )
+
+
+reference_input_variable(
+    column = IntCol,
+    entity_class = Individus,
+    is_permanent = True,
+    label = u"Rôle dans le ménage",
+    name = 'role_dans_menage',
+    )
+
+
+reference_input_variable(
+    column = FloatCol,
+    entity_class = Individus,
+    label = "Salaire brut",
+    name = 'salaire_brut',
+    )
+
+
+# Calculated variables
+
+
+reference_formula = make_reference_formula_decorator(entity_class_by_symbol = entity_class_by_symbol)
+
+
+@reference_formula
+class age(SimpleFormulaColumn):
+    column = AgeCol
+    entity_class = Individus
+    label = u"Revenu disponible de l'individu"
+
+    def function(birth, period):
+        return (np.datetime64(period.date) - birth).astype('timedelta64[Y]')
+
+    def get_output_period(self, period):
+        return period.start.period(u'year').offset('first-of')
---
+++ openfisca_france_indirect_taxation/entities.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import collections
+import itertools
+
+from openfisca_core import entities
+
+
+class Individus(entities.AbstractEntity):
+    column_by_name = collections.OrderedDict()
+    is_persons_entity = True
+    key_plural = 'individus'
+    key_singular = 'individu'
+    label = u'Personne'
+    name_key = 'nom_individu'
+    symbol = 'ind'
+
+
+class Menages(entities.AbstractEntity):
+    column_by_name = collections.OrderedDict()
+    index_for_person_variable_name = 'idmen'
+    key_plural = 'menages'
+    key_singular = 'menage'
+    label = u'Logement principal'
+    max_cardinality_by_role_key = {'conjoint': 1, 'personne_de_reference': 1}
+    name_key = 'nom_menage'
+    role_for_person_variable_name = 'quimen'
+    roles_key = ['personne_de_reference', 'conjoint', 'enfants', 'autres']
+    label_by_role_key = {
+        'autres': u'Autres',
+        'conjoint': u'Conjoint',
+        'enfants': u'Enfants',
+        'personne_de_reference': u'Personne de référence',
+        }
+    symbol = 'men'
+
+    def iter_member_persons_role_and_id(self, member):
+        role = 0
+
+        personne_de_reference_id = member['personne_de_reference']
+        assert personne_de_reference_id is not None
+        yield role, personne_de_reference_id
+        role += 1
+
+        conjoint_id = member.get('conjoint')
+        if conjoint_id is not None:
+            yield role, conjoint_id
+        role += 1
+
+        autres_id = member.get('autres') or []
+        enfants_id = member.get('enfants') or []
+        for enfant_role, enfant_id in enumerate(itertools.chain(enfants_id, autres_id), role):
+            yield enfant_role, enfant_id
+
+
+entity_class_by_symbol = dict(
+    ind = Individus,
+    men = Menages,
+    )
