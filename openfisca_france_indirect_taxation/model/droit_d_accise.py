92cb70dc442e4802de5d4cb9f7b7c584f220775a
Etienne Fontaine <etienne_fontaine@hotmail.fr>
2015-03-09 15:21:50 +0100

    droits d accises assurance

--- openfisca_france_indirect_taxation/model/droit_d_accise.py
+++ openfisca_france_indirect_taxation/model/droit_d_accise.py
@@ -23,7 +23,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-def montant_droit_d_accise_alcool(depense, droit_cn, consommation_cn, taux_plein_tva):
+def montant_droit_d_accise(depense, droit_cn, consommation_cn, taux_plein_tva):
     """
     Calcule le montant de droit d'accise sur un volume de dépense payé pour le poste adéquat
     """
