621cb766af88c1f0655793841963fa9a013dd56a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-04-26 18:50:54 +0200

    Fix some erroneous examples

--- openfisca_france_indirect_taxation/example/df_estimation_model.py
+++ openfisca_france_indirect_taxation/example/df_estimation_model.py
@@ -26,23 +26,21 @@ Created on Sat Apr 25 17:00:01 2015
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import os
-import logging
-import numpy
-import pandas
+
 
 from __future__ import division
 
+
+import logging
+
+
 from pandas import DataFrame
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
 import openfisca_france_indirect_taxation
 from openfisca_survey_manager.survey_collections import SurveyCollection
 
 from openfisca_france_data import default_config_files_directory as config_files_directory
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
-from openfisca_france_data.temporary import TemporaryStore
 
 
 def get_input_data_frame(year):
@@ -77,11 +75,9 @@ def simulate_df(var_to_be_simulated, year = 2000):
 
 
 if __name__ == '__main__':
-    import logging
     log = logging.getLogger(__name__)
     import sys
     logging.basicConfig(level = logging.INFO, stream = sys.stdout)
-
 
     list_coicop12 = []
     for coicop12_index in range(1, 13):
