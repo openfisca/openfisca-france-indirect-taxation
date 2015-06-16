1a762527c83ebc054402b61ebb68b57672309890
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-06-16 17:03:40 +0200

    Utils for the examples
    Useful functions gathered here instead of in each script

---
+++ openfisca_france_indirect_taxation/example/utils_example.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 16:19:14 2015
+
+@author: thomas.douenne
+"""
+
+from __future__ import division
+
+from pandas import DataFrame, concat
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+import openfisca_france_indirect_taxation
+from openfisca_survey_manager.survey_collections import SurveyCollection
+
+
+from openfisca_france_data import default_config_files_directory as config_files_directory
+from openfisca_france_indirect_taxation.surveys import SurveyScenario
+
+
+def get_input_data_frame(year):
+    openfisca_survey_collection = SurveyCollection.load(
+        collection = "openfisca_indirect_taxation", config_files_directory = config_files_directory)
+    openfisca_survey = openfisca_survey_collection.get_survey("openfisca_indirect_taxation_data_{}".format(year))
+    input_data_frame = openfisca_survey.get_values(table = "input")
+    input_data_frame.reset_index(inplace = True)
+    return input_data_frame
+
+
+def simulate_df(var_to_be_simulated, year):
+    '''
+    Construction de la DataFrame à partir de laquelle sera faite l'analyse des données
+    '''
+    input_data_frame = get_input_data_frame(year)
+    TaxBenefitSystem = openfisca_france_indirect_taxation.init_country()
+
+    tax_benefit_system = TaxBenefitSystem()
+    survey_scenario = SurveyScenario().init_from_data_frame(
+        input_data_frame = input_data_frame,
+        tax_benefit_system = tax_benefit_system,
+        year = year,
+        )
+    simulation = survey_scenario.new_simulation()
+    return DataFrame(
+        dict([
+            (name, simulation.calculate(name)) for name in var_to_be_simulated
+
+            ])
+        )
+
+
+def wavg(groupe, var):
+    '''
+    Fonction qui calcule la moyenne pondérée par groupe d'une variable
+    '''
+    d = groupe[var]
+    w = groupe['pondmen']
+    return (d * w).sum() / w.sum()
+
+
+def collapse(dataframe, groupe, var):
+    '''
+    Pour une variable, fonction qui calcule la moyenne pondérée au sein de chaque groupe.
+    '''
+    grouped = dataframe.groupby([groupe])
+    var_weighted_grouped = grouped.apply(lambda x: wavg(groupe = x, var = var))
+    return var_weighted_grouped
+
+
+def df_weighted_average_grouped(dataframe, groupe, varlist):
+    '''
+    Agrège les résultats de weighted_average_grouped() en une unique dataframe pour la liste de variable 'varlist'.
+    '''
+    return DataFrame(
+        dict([
+            (var, collapse(dataframe, groupe, var)) for var in varlist
+            ])
+        )
