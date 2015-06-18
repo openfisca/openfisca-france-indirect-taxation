60f39c99f4e9560f9b9d4a20703891695e5d8c8c
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-06-18 15:24:06 +0200

    Fix issue with the plot. Now the 9 plots can be seen instead of 1. Legends are created for easier interpretation.

--- openfisca_france_indirect_taxation/example/plot_taux_effort.py
+++ openfisca_france_indirect_taxation/example/plot_taux_effort.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
         'ident_men',
         'pondmen',
         'decuc',
-        'decile',
+        'niveau_vie_decile',
         'revtot',
         'somme_coicop12_conso',
         'ocde10',
@@ -63,17 +63,6 @@ if __name__ == '__main__':
         'rev_disponible',
         'rev_disp_loyerimput',
         'montant_total_taxes_indirectes' # ,
-#        'montant_tva_total',
-#        'montant_droit_d_accise_vin',
-#        'montant_droit_d_accise_biere',
-#        'montant_droit_d_accise_alcools_forts',
-#        'montant_droit_d_accise_cigarette',
-#        'montant_droit_d_accise_cigares',
-#        'montant_droit_d_accise_tabac_a_rouler',
-#        'montant_taxe_assurance_transport',
-#        'montant_taxe_assurance_sante',
-#        'montant_taxe_autres_assurances',
-#        'montant_tipp'
         ]
 
 # 1 calcul taux d'effort sur le revenu total
@@ -81,26 +70,34 @@ if __name__ == '__main__':
     for year in [2000, 2005, 2011]:
         df = simulate_df(var_to_be_simulated = var_to_be_simulated, year = year)
         if year == 2011:
-            df1.decile[df1.decuc == 10] = 10
+            df.niveau_vie_decile[df.decuc == 10] = 10
         varlist = ['revtot', 'montant_total_taxes_indirectes', 'rev_disponible', 'rev_disp_loyerimput']
-        Wconcat1 = df_weighted_average_grouped(dataframe = df, groupe = 'decile', varlist = varlist)
-        Wconcat1['taux_d_effort'] = Wconcat1['montant_total_taxes_indirectes'] / Wconcat1['revtot']
-        df_to_graph1 = Wconcat1['taux_d_effort']
+        Wconcat1 = df_weighted_average_grouped(dataframe = df, groupe = 'niveau_vie_decile', varlist = varlist)
+        Wconcat1['taux_d_effort_revtot_{}'.format(year)] = Wconcat1['montant_total_taxes_indirectes'] / Wconcat1['revtot']
+        df_to_graph1 = Wconcat1['taux_d_effort_revtot_{}'.format(year)]
 
         df.rev_disponible = df.rev_disponible * 1.33
-        Wconcat2 = df_weighted_average_grouped(dataframe = df, groupe = 'decile', varlist = varlist)
-        Wconcat2['taux_d_effort'] = Wconcat2['montant_total_taxes_indirectes'] / Wconcat2['rev_disponible']
-        df_to_graph2 = Wconcat2['taux_d_effort']
+        Wconcat2 = df_weighted_average_grouped(dataframe = df, groupe = 'niveau_vie_decile', varlist = varlist)
+        Wconcat2['taux_d_effort_rev_disponible_{}'.format(year)] = Wconcat2['montant_total_taxes_indirectes'] / Wconcat2['rev_disponible']
+        df_to_graph2 = Wconcat2['taux_d_effort_rev_disponible_{}'.format(year)]
 
-        Wconcat3 = df_weighted_average_grouped(dataframe = df, groupe = 'decile', varlist = varlist)
-        Wconcat3['taux_d_effort'] = Wconcat3['montant_total_taxes_indirectes'] / Wconcat3['rev_disp_loyerimput']
-        df_to_graph3 = Wconcat3['taux_d_effort']
+        Wconcat3 = df_weighted_average_grouped(dataframe = df, groupe = 'niveau_vie_decile', varlist = varlist)
+        Wconcat3['taux_d_effort_rev_disp_loyerimput_{}'.format(year)] = Wconcat3['montant_total_taxes_indirectes'] / Wconcat3['rev_disp_loyerimput']
+        df_to_graph3 = Wconcat3['taux_d_effort_rev_disp_loyerimput_{}'.format(year)]
 
-        df_to_graph1.plot(kind = 'bar', stacked = True)
+        axes1 = df_to_graph1.plot(stacked = True)
         plt.axhline(0, color = 'k')
 
-        df_to_graph2.plot(kind = 'bar', stacked = True)
+        axes2 = df_to_graph2.plot(stacked = True)
         plt.axhline(0, color = 'k')
 
-        df_to_graph3.plot(kind = 'bar', stacked = True)
+        axes3 = df_to_graph3.plot(stacked = True)
         plt.axhline(0, color = 'k')
+
+        axes1.yaxis.set_major_formatter(ticker.FuncFormatter(percent_formatter))
+
+        axes1.legend(
+            bbox_to_anchor = (1.80, 0.95),
+            )
+
+    plt.show()
