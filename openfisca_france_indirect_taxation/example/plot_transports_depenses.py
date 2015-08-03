af4c5e5ec2236bbf6e4b5f6ad4b4654ce5570690
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-03 17:36:51 +0200

    Can choose color in graphs.

--- openfisca_france_indirect_taxation/example/plot_transports_depenses.py
+++ openfisca_france_indirect_taxation/example/plot_transports_depenses.py
@@ -6,7 +6,8 @@ Created on Fri Jul 24 11:07:05 2015
 """
 
 from ipp_macro_series_parser.agregats_transports.transports_cleaner import a3_a
-from openfisca_france_indirect_taxation.example.utils_example import graph_builder_carburants
+from openfisca_france_indirect_taxation.example.utils_example import graph_builder_carburants, \
+    graph_builder_carburants_no_color
 
 a3_a['to_be_used'] = 0
 a3_a.loc[a3_a['index'] == u'0722 Carburants et lubrifiants (1)', 'to_be_used'] = 1
@@ -22,22 +23,22 @@ depenses_menages_transports = depenses_menages_transports.drop(['to_be_used'] + 
 depenses_menages_transports = depenses_menages_transports.set_index(['index'])
 depenses_menages_transports = depenses_menages_transports.transpose()
 
-depenses_menages_transports['part_carburants_depenses_totales'] = (
+depenses_menages_transports[u'part carburants dépenses totales'] = (
     depenses_menages_transports[u'Dépenses carburants et lubrifiants'] /
     depenses_menages_transports[u'Ensemble des dépenses de consommation des ménages ']
     )
-depenses_menages_transports['part_transports_depenses_totales'] = (
+depenses_menages_transports[u'part transports dépenses totales'] = (
     depenses_menages_transports[u'Dépenses totales en transports'] /
     depenses_menages_transports[u'Ensemble des dépenses de consommation des ménages ']
     )
 
 print 'Evolution des dépenses des ménages en carburants'
-graph_builder_carburants(depenses_menages_transports[u'Dépenses carburants et lubrifiants'],
-    'depenses_menages_carburants')
+graph_builder_carburants_no_color(depenses_menages_transports[u'Dépenses carburants et lubrifiants'],
+    'depenses menages carburants', 0.9, 0.20)
 
 print 'Evolution de la part des carburants et des transports dans les dépenses totales des ménages'
 graph_builder_carburants(
-    depenses_menages_transports[['part_carburants_depenses_totales'] + ['part_transports_depenses_totales']],
-    'part_transports_depenses_menages')
+    depenses_menages_transports[[u'part transports dépenses totales'] + [u'part carburants dépenses totales']],
+    'part transports depenses menages', 1, 0.65, 'midnightblue', 'turquoise', 'blue')
 
 # Evolution de la part de la TICPE dans les dépenses des ménages : besoin de parser les barèmes
--- openfisca_france_indirect_taxation/example/plot_transports_quantites.py
+++ openfisca_france_indirect_taxation/example/plot_transports_quantites.py
@@ -6,10 +6,12 @@ Created on Fri Jul 24 09:46:35 2015
 """
 
 from ipp_macro_series_parser.agregats_transports.transports_cleaner import a6_b, g2_1, g_3a
-from openfisca_france_indirect_taxation.example.utils_example import graph_builder_carburants
+from openfisca_france_indirect_taxation.example.utils_example import graph_builder_carburants, \
+    graph_builder_carburants_no_color
 
 recettes_ticpe = a6_b[a6_b['categorie'] == u'TICPE ( TIPP avant 2010) (1)']
 recettes_ticpe = recettes_ticpe[recettes_ticpe['index'] == 'Total']
+del recettes_ticpe['index']
 
 g_3a.loc[g_3a['index'] == u'Voitures particulières', 'index'] = 'to_be_used'
 g_3a.loc[g_3a['index'] == u'Total VP françaises', 'index'] = 'to_be_used'
@@ -17,21 +19,30 @@ quantite_conso_vp = g_3a[g_3a['index'] == 'to_be_used']
 del quantite_conso_vp['index']
 
 taille_parc_vp = g2_1[g2_1['categorie'] == u'Voitures particulières']
+del taille_parc_vp['categorie']
 
-recettes_ticpe.set_index(['categorie', 'index'], inplace = True)
+recettes_ticpe.set_index(['categorie'], inplace = True)
 recettes_ticpe = recettes_ticpe.transpose()
-taille_parc_vp.set_index(['categorie', 'index'], inplace = True)
+taille_parc_vp.set_index(['index'], inplace = True)
 taille_parc_vp = taille_parc_vp.transpose()
 quantite_conso_vp.set_index(['categorie'], inplace = True)
 quantite_conso_vp = quantite_conso_vp.transpose()
 
+recettes_ticpe.rename(columns = {u'TICPE ( TIPP avant 2010) (1)': 'Total recettes TICPE'}, inplace = True)
+
+taille_parc_vp.rename(columns = {'Total': 'parc vp total', 'dont Diesel': 'dont diesel'}, inplace = True)
+
+quantite_conso_vp = quantite_conso_vp[['ESSENCE + GAZOLE'] + ['ESSENCE'] + ['GAZOLE']]
+quantite_conso_vp.rename(columns = {'ESSENCE + GAZOLE': 'consommation totale', 'ESSENCE': 'consommation essence',
+    'GAZOLE': 'consommation diesel'}, inplace = True)
+
 print 'Evolution de la taille du parc automobile au cours du temps'
-graph_builder_carburants(taille_parc_vp, 'taille_parc_vp')
+graph_builder_carburants(taille_parc_vp, 'taille parc vp', 1, 0.3, 'blue', 'green', 'red')
 
 print 'Evolution des quantités de carburants consommées par les voitures particulières françaises'
-graph_builder_carburants(quantite_conso_vp, 'quantite_conso_vp')
+graph_builder_carburants(quantite_conso_vp, 'quantite conso vp', 0.85, 0.3, 'blue', 'green', 'red')
 
 print 'Evolution des recettes totales issues de la TICPE'
-graph_builder_carburants(recettes_ticpe, 'recettes_totales_ticpe')
+graph_builder_carburants_no_color(recettes_ticpe, 'recettes totales ticpe', 1, 0.17)
 
 # (pour avoir les recettes prélevées sur les ménages, parser les barèmes et multiplier les accises par les quantités)
