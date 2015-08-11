9498230e1641cd7786f584f64526143588da7131
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-11 18:30:45 +0200

    Save dictionary as csv in assets.

--- openfisca_france_indirect_taxation/scripts/build_prix_carburants.py
+++ openfisca_france_indirect_taxation/scripts/build_prix_carburants.py
@@ -6,6 +6,9 @@ Created on Tue Aug 11 16:05:22 2015
 """
 
 import sys
+import pkg_resources
+import os
+import pandas as pd
 
 
 def main():
@@ -16,7 +19,7 @@ def main():
     for element in ['diesel_ttc']:
         prix_annuel_carburants = prix_annuel_carburants_90_14['{}'.format(element)]
         all_values['{}'.format(element)] = []
-        prix_carbu = {
+        prix_carburants = {
             "@type": "Node",
             "description": "prix des carburants en euros par hectolitre",
             "children": {},
@@ -28,12 +31,22 @@ def main():
             values['value'] = prix_annuel_carburants.loc[year] * 100
             all_values[element].append(values)
 
-        prix_carbu['children'][element] = {
+        prix_carburants['children'][element] = {
             "@type": "Parameter",
             "description": element.replace('_', ' '),
             "format": "float",
             "values": all_values[element]
             }
+
+        assets_directory = os.path.join(
+            pkg_resources.get_distribution('openfisca_france_indirect_taxation').location
+            )
+
+        prix_annuel_carburants = pd.DataFrame(prix_carburants.items())
+        prix_annuel_carburants.to_csv(os.path.join(
+            assets_directory, 'openfisca_france_indirect_taxation', 'assets', 'prix_annuel_carburants.csv')
+            )
+
 
 if __name__ == "__main__":
     sys.exit(main())
