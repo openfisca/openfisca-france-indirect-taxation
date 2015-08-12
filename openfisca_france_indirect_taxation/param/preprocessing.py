b3d9eefaf709e5b5cfa1a528d30bc4fee04fa247
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-12 14:43:42 +0200

    Rename to differentiate list from dictionary

--- openfisca_france_indirect_taxation/param/preprocessing.py
+++ openfisca_france_indirect_taxation/param/preprocessing.py
@@ -36,7 +36,7 @@ def preprocess_legislation(legislation_json):
 
     default_config_files_directory = os.path.join(
         pkg_resources.get_distribution('openfisca_france_indirect_taxation').location)
-    prix_carburants = pd.read_csv(
+    prix_annuel_carburants = pd.read_csv(
         os.path.join(
             default_config_files_directory,
             'openfisca_france_indirect_taxation',
@@ -44,13 +44,13 @@ def preprocess_legislation(legislation_json):
             'prix_annuel_carburants.csv'
             ), sep =';'
         )
-    prix_carburants['Date'] = prix_carburants['Date'].astype(int)
-    prix_carburants = prix_carburants.set_index('Date')
+    prix_annuel_carburants['Date'] = prix_annuel_carburants['Date'].astype(int)
+    prix_annuel_carburants = prix_annuel_carburants.set_index('Date')
 
     all_values = {}
     for element in ['diesel_ht', 'diesel_ttc', 'super_95_ht', 'super_95_ttc', 'super_98_ht', 'super_98_ttc',
             'super_95_e10_ht', 'super_95_e10_ttc', 'gplc_ht', 'gplc_ttc', 'super_plombe_ht', 'super_plombe_ttc']:
-        prix_carburants = prix_carburants['{}'.format(element)]
+        prix_annuel_carburants = prix_annuel_carburants['{}'.format(element)]
         all_values['{}'.format(element)] = []
         prix_carburants = {
             "@type": "Node",
@@ -61,7 +61,7 @@ def preprocess_legislation(legislation_json):
             values = dict()
             values['start'] = u'{}-01-01'.format(year)
             values['stop'] = u'{}-12-31'.format(year)
-            values['value'] = prix_carburants.loc[year] * 100
+            values['value'] = prix_annuel_carburants.loc[year] * 100
             all_values[element].append(values)
 
         prix_carburants['children'][element] = {
