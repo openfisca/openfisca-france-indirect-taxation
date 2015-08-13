aab90b7067e0988fd99b7de8f0203cedbc677ee1
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-13 11:56:06 +0200

    Modifies functions to fit with legislation

--- openfisca_france_indirect_taxation/model/taxes_indirectes/montant_taxes_indirectes.py
+++ openfisca_france_indirect_taxation/model/taxes_indirectes/montant_taxes_indirectes.py
@@ -26,6 +26,7 @@ from __future__ import division
 
 
 import datetime
+from datetime import date
 
 from ..base import *  # noqa analysis:ignore
 
@@ -83,14 +84,33 @@ class taxe_assurance_sante(SimpleFormulaColumn):
 
 
 @reference_formula
-class taxe_assurance_transport(SimpleFormulaColumn):
+class taxe_assurance_transport(DatedFormulaColumn):
     column = FloatCol
     entity_class = Menages
     label = u"Montant des taxes sur l'assurance transport"
 
+    @dated_function(start = date(1984, 1, 1), stop = date(2001, 12, 31))
     def function(self, simulation, period):
         consommation_assurance_transport = simulation.calculate('consommation_assurance_transport', period)
-        taux = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.taux_assur_transport
+        taux_assurance_vtm = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.assurance_pour_les_vehicules_terrestres_a_moteurs_pour_les_particuliers
+        taux = taux_assurance_vtm
+        return period, tax_from_expense_including_tax(consommation_assurance_transport, taux)
+
+    @dated_function(start = date(2002, 1, 1), stop = date(2004, 8, 4))
+    def function(self, simulation, period):
+        consommation_assurance_transport = simulation.calculate('consommation_assurance_transport', period)
+        taux_assurance_vtm = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.assurance_pour_les_vehicules_terrestres_a_moteurs_pour_les_particuliers
+        taux_contrib_secu_vtm = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.contribution_secu_assurances_automobiles
+        taux = taux_assurance_vtm + taux_contrib_secu_vtm
+        return period, tax_from_expense_including_tax(consommation_assurance_transport, taux)
+
+    @dated_function(start = date(2004, 8, 5), stop = date(2015, 12, 31))
+    def function(self, simulation, period):
+        consommation_assurance_transport = simulation.calculate('consommation_assurance_transport', period)
+        taux_assurance_vtm = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.assurance_pour_les_vehicules_terrestres_a_moteurs_pour_les_particuliers
+        taux_contrib_secu_vtm = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.contribution_secu_assurances_automobiles
+        taux_contrib_fgao = simulation.legislation_at(period.start).imposition_indirecte.fgao.contribution_des_assures_en_pourcentage_des_primes
+        taux = taux_assurance_vtm + taux_contrib_secu_vtm + taux_contrib_fgao
         return period, tax_from_expense_including_tax(consommation_assurance_transport, taux)
 
 
@@ -102,7 +122,7 @@ class taxe_autres_assurances(SimpleFormulaColumn):
 
     def function(self, simulation, period):
         consommation_autres_assurances = simulation.calculate('consommation_autres_assurances', period)
-        taux = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.taux_assurances_autres
+        taux = simulation.legislation_at(period.start).imposition_indirecte.taux_assurances.autres_assurances
         return period, tax_from_expense_including_tax(consommation_autres_assurances, taux)
 
 
@@ -128,7 +148,7 @@ class droit_d_accise_cigares(SimpleFormulaColumn):
 
     def function(self, simulation, period):
         consommation_cigares = simulation.calculate('consommation_cigares', period)
-        taux_normal_cigare = simulation.legislation_at(period.start).imposition_indirecte.tabac.cigares.taux_normal_cigare
+        taux_normal_cigare = simulation.legislation_at(period.start).imposition_indirecte.tabac.taux_normal.cigares
         return period, tax_from_expense_including_tax(consommation_cigares, taux_normal_cigare)
 
 
@@ -140,7 +160,7 @@ class droit_d_accise_cigarette(SimpleFormulaColumn):
 
     def function(self, simulation, period):
         consommation_cigarette = simulation.calculate('consommation_cigarette', period)
-        taux_normal_cigarette = simulation.legislation_at(period.start).imposition_indirecte.tabac.cigarettes.taux_normal_cigarette
+        taux_normal_cigarette = simulation.legislation_at(period.start).imposition_indirecte.tabac.taux_normal.cigarettes
         return period, tax_from_expense_including_tax(consommation_cigarette, taux_normal_cigarette)
 
 
@@ -179,7 +199,7 @@ class droit_d_accise_tabac_a_rouler(SimpleFormulaColumn):
 
     def function(self, simulation, period):
         consommation_tabac_a_rouler = simulation.calculate('consommation_tabac_a_rouler', period)
-        taux_normal_tabac_a_rouler = simulation.legislation_at(period.start).imposition_indirecte.tabac.tabac_a_rouler.taux_normal_tabac_a_rouler
+        taux_normal_tabac_a_rouler = simulation.legislation_at(period.start).imposition_indirecte.tabac.taux_normal.tabac_a_rouler
         return period, tax_from_expense_including_tax(consommation_tabac_a_rouler, taux_normal_tabac_a_rouler)
 
 
