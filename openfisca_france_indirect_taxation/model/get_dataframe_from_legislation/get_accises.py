ebfa6588374a9079a6facadf942e9994336cf4ee
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-17 19:01:54 +0200

    Build dataframe from legislation_json

---
+++ openfisca_france_indirect_taxation/model/get_dataframe_from_legislation/get_accises.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 18:09:46 2015
+
+@author: thomas.douenne
+"""
+
+import pandas as pd
+from pandas import concat
+
+import openfisca_france_indirect_taxation
+
+TaxBenefitSystem = openfisca_france_indirect_taxation.init_country()
+tax_benefit_system = TaxBenefitSystem()
+legislation_json = tax_benefit_system.legislation_json
+
+
+def get_accises_carburants(liste_carburants):
+    accises_carburants = None
+    for element in liste_carburants:
+        accise_dict = \
+            legislation_json['children']['imposition_indirecte']['children']['ticpe']['children'][element]
+        df_accise = pd.DataFrame.from_dict(accise_dict['values'])
+
+        df_accise['start'] = df_accise['start'].str[:4]
+        del df_accise['stop']
+        df_accise = df_accise.set_index('start')
+        df_accise = df_accise.transpose()
+
+        for year in range(1993, 2017):
+            try:
+                df_accise['{}'.format(year)]
+            except:
+                df_accise['{}'.format(year)] = df_accise['{}'.format(year - 1)]
+
+        for year in range(1993, 2002):
+            df_accise['{}'.format(year)] = df_accise['{}'.format(year)] / 6.55957
+
+        df_accise = df_accise.transpose()
+        df_accise.rename(columns = {'value': 'accise ' + element.replace('_', ' ')}, inplace = True)
+        df_accise = df_accise.sort_index()
+
+        if accises_carburants is not None:
+            accises_carburants = concat([accises_carburants, df_accise], axis=1)
+        else:
+            accises_carburants = df_accise
+
+    return accises_carburants
---
+++ openfisca_france_indirect_taxation/model/get_dataframe_from_legislation/get_tva.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 18:20:34 2015
+
+@author: thomas.douenne
+"""
+
+import pandas as pd
+from pandas import concat
+
+import openfisca_france_indirect_taxation
+
+TaxBenefitSystem = openfisca_france_indirect_taxation.init_country()
+tax_benefit_system = TaxBenefitSystem()
+legislation_json = tax_benefit_system.legislation_json
+
+
+def get_tva_taux_plein():
+    dict_tva_taux_plein = \
+        legislation_json['children']['imposition_indirecte']['children']['tva']['children']['taux_plein']
+    df_tva_taux_plein = pd.DataFrame.from_dict(dict_tva_taux_plein['values'])
+    df_tva_taux_plein['start'] = df_tva_taux_plein['start'].str[:4]
+    del df_tva_taux_plein['stop']
+    df_tva_taux_plein = df_tva_taux_plein.set_index('start')
+    df_tva_taux_plein = df_tva_taux_plein.transpose()
+    for year in range(1968, 2015):
+        try:
+            df_tva_taux_plein['{}'.format(year)]
+        except:
+            df_tva_taux_plein['{}'.format(year)] = df_tva_taux_plein['{}'.format(year - 1)]
+    df_tva_taux_plein = df_tva_taux_plein.transpose()
+    df_tva_taux_plein = df_tva_taux_plein.sort_index()
+
+    return df_tva_taux_plein
