2b0d189f62c1ad5e2056e8f6b289872539eab0b7
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-18 17:14:01 +0200

    Include regional upward adjustment of ticpe

--- openfisca_france_indirect_taxation/example/example_ticpe/plot_ticpe_accises.py
+++ openfisca_france_indirect_taxation/example/example_ticpe/plot_ticpe_accises.py
@@ -7,11 +7,12 @@ Created on Mon Aug 17 15:48:31 2015
 
 
 from openfisca_france_indirect_taxation.example.utils_example import graph_builder_bar_list
-from openfisca_france_indirect_taxation.model.get_dataframe_from_legislation.get_accises import get_accises_carburants
+from openfisca_france_indirect_taxation.model.get_dataframe_from_legislation.get_accises import \
+    get_accise_ticpe_majoree
 
 liste = ['ticpe_gazole', 'ticpe_super9598', 'super_plombe_ticpe']
-df_accises = get_accises_carburants(liste)
+df_accises = get_accise_ticpe_majoree()
 
-graph_builder_bar_list(df_accises['accise ticpe gazole'])
-graph_builder_bar_list(df_accises['accise ticpe super9598'])
-graph_builder_bar_list(df_accises['accise super plombe ticpe'])
+graph_builder_bar_list(df_accises['accise majoree sans plomb'])
+graph_builder_bar_list(df_accises['accise majoree diesel'])
+graph_builder_bar_list(df_accises['accise majoree super plombe'])
--- openfisca_france_indirect_taxation/model/get_dataframe_from_legislation/get_accises.py
+++ openfisca_france_indirect_taxation/model/get_dataframe_from_legislation/get_accises.py
@@ -15,6 +15,7 @@ tax_benefit_system = TaxBenefitSystem()
 legislation_json = tax_benefit_system.legislation_json
 
 
+# To do: integrate "majoration regionale ticpe" to reflect the real excise taxes applied
 def get_accises_carburants(liste_carburants):
     accises_carburants = None
     for element in liste_carburants:
@@ -43,3 +44,53 @@ def get_accises_carburants(liste_carburants):
             accises_carburants = df_accise
 
     return accises_carburants
+
+
+def get_majoration_ticpe(liste_carburants):
+    majoration_carburants = None
+    for element in liste_carburants:
+        majoration_dict = \
+            legislation_json['children']['imposition_indirecte']['children'][element]['children']['alsace']
+        df_majoration = pd.DataFrame.from_dict(majoration_dict['values'])
+        df_majoration['start'] = df_majoration['start'].str[:4]
+        del df_majoration['stop']
+        df_majoration = df_majoration.set_index('start')
+        df_majoration = df_majoration.transpose()
+
+        for year in range(2007, 2021):
+            try:
+                df_majoration['{}'.format(year)]
+            except:
+                df_majoration['{}'.format(year)] = df_majoration['{}'.format(year - 1)]
+        df_majoration = df_majoration.transpose()
+        df_majoration.rename(columns = {'value': element.replace('_', ' ')}, inplace = True)
+        df_majoration = df_majoration.sort_index()
+
+        if majoration_carburants is not None:
+            majoration_carburants = concat([majoration_carburants, df_majoration], axis = 1)
+        else:
+            majoration_carburants = df_majoration
+
+    return majoration_carburants
+
+
+def get_accise_ticpe_majoree():
+    accise_nationale = get_accises_carburants(['ticpe_gazole', 'ticpe_super9598', 'super_plombe_ticpe'])
+    majoration_regionale = get_majoration_ticpe(['major_regionale_ticpe_gazole', 'major_regionale_ticpe_super'])
+    accise_totale = concat([accise_nationale, majoration_regionale], axis = 1)
+    accise_totale = accise_totale.fillna(0)
+    accise_totale['accise majoree sans plomb'] = (
+        accise_totale['accise ticpe super9598'] + accise_totale['major regionale ticpe super']
+        )
+    accise_totale['accise majoree super plombe'] = (
+        accise_totale['accise super plombe ticpe'] + accise_totale['major regionale ticpe super']
+        )
+    accise_totale['accise majoree diesel'] = (
+        accise_totale['accise ticpe gazole'] + accise_totale['major regionale ticpe gazole']
+        )
+    accise_totale = accise_totale[
+        ['accise majoree sans plomb'] + ['accise majoree super plombe'] + ['accise majoree diesel']
+        ].copy()
+    accise_totale = accise_totale[accise_totale['accise majoree sans plomb'] != 2.5].copy()
+
+    return accise_totale
