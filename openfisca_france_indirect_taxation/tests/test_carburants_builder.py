e025500c1ebc4393294ff3189d667c3733cb82dc
thomasdouenne <thomas.douenne@sciencespo.fr>
2015-08-24 17:19:28 +0200

    Adapt variables to new names in scripts

--- openfisca_france_indirect_taxation/tests/test_carburants_builder.py
+++ openfisca_france_indirect_taxation/tests/test_carburants_builder.py
@@ -15,21 +15,29 @@ from openfisca_france_indirect_taxation.example.utils_example import get_input_d
 for year in [2000, 2005, 2011]:
     aggregates_data_frame = get_input_data_frame(year)
     if year == 2000:
-        df = aggregates_data_frame[['07220'] + ['coicop12_7'] + ['07110'] + ['07120'] + ['07130'] + ['07210'] +
-            ['07230'] + ['07240'] + ['07310'] + ['07320'] + ['07330'] + ['07340'] + ['07360']]
+        df = aggregates_data_frame[['poste_coicop_722'] + ['coicop12_7'] + ['poste_coicop_711'] +
+            ['poste_coicop_712'] + ['poste_coicop_713'] + ['poste_coicop_721'] + ['poste_coicop_723'] +
+            ['poste_coicop_724'] + ['poste_coicop_731'] + ['poste_coicop_732'] + ['poste_coicop_733'] +
+            ['poste_coicop_734'] + ['poste_coicop_736']]
         df['check'] = (
-            df['07110'] + df['07120'] + df['07130'] + df['07210'] + df['07220'] + df['07230'] + df['07240'] +
-            df['07310'] + df['07320'] + df['07330'] + df['07340'] + df['07360'] - df['coicop12_7']
+            df['poste_coicop_711'] + df['poste_coicop_712'] + df['poste_coicop_713'] + df['poste_coicop_721'] +
+            df['poste_coicop_722'] + df['poste_coicop_723'] + df['poste_coicop_724'] + df['poste_coicop_731'] +
+            df['poste_coicop_732'] + df['poste_coicop_733'] + df['poste_coicop_734'] + df['poste_coicop_736'] -
+            df['coicop12_7']
             ).copy()
     else:
-        df = aggregates_data_frame[['07220'] + ['coicop12_7'] + ['07110'] + ['07120'] + ['07350'] + ['07130'] +
-            ['07210'] + ['07230'] + ['07240'] + ['07310'] + ['07320'] + ['07330'] + ['07340'] + ['07360']]
+        df = aggregates_data_frame[['poste_coicop_722'] + ['coicop12_7'] + ['poste_coicop_711'] +
+            ['poste_coicop_712'] + ['poste_coicop_735'] + ['poste_coicop_713'] + ['poste_coicop_721'] +
+            ['poste_coicop_723'] + ['poste_coicop_724'] + ['poste_coicop_731'] + ['poste_coicop_732'] +
+            ['poste_coicop_733'] + ['poste_coicop_734'] + ['poste_coicop_736']]
         df['check'] = (
-            df['07110'] + df['07120'] + df['07130'] + df['07210'] + df['07220'] + df['07230'] + df['07240'] +
-            df['07310'] + df['07320'] + df['07330'] + df['07340'] + df['07360'] + df['07350'] - df['coicop12_7']
+            df['poste_coicop_711'] + df['poste_coicop_712'] + df['poste_coicop_713'] + df['poste_coicop_721'] +
+            df['poste_coicop_722'] + df['poste_coicop_723'] + df['poste_coicop_724'] + df['poste_coicop_731'] +
+            df['poste_coicop_732'] + df['poste_coicop_733'] + df['poste_coicop_734'] + df['poste_coicop_736'] +
+            df['poste_coicop_735'] - df['coicop12_7']
             ).copy()
     assert_near(df['check'].any(), 0, 0.0001), "the total of transport differs from the sum of its components"
 
-    df2 = aggregates_data_frame[['07220'] + ['categorie_fiscale_14']]
-    df2['check'] = df2['07220'] - df2['categorie_fiscale_14']
+    df2 = aggregates_data_frame[['poste_coicop_722'] + ['categorie_fiscale_14']]
+    df2['check'] = df2['poste_coicop_722'] - df2['categorie_fiscale_14']
     assert_near(df2['check'].any(), 0, 0.0001), "categorie_fiscale_14 is not correctly constructed"
