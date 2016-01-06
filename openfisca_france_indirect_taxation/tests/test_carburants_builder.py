2ec6c25b7981654e6dfd02400c3f33c96ac311fe
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-01-06 14:29:59 +0100

    Remove nomre relevant test Clean

--- openfisca_france_indirect_taxation/tests/test_carburants_builder.py
+++ openfisca_france_indirect_taxation/tests/test_carburants_builder.py
@@ -15,10 +15,10 @@ from openfisca_france_indirect_taxation.example.utils_example import get_input_d
 for year in [2000, 2005, 2011]:
     aggregates_data_frame = get_input_data_frame(year)
     if year == 2000:
-        df = aggregates_data_frame[['poste_coicop_722'] + ['coicop12_7'] + ['poste_coicop_711'] +
-            ['poste_coicop_712'] + ['poste_coicop_713'] + ['poste_coicop_721'] + ['poste_coicop_723'] +
-            ['poste_coicop_724'] + ['poste_coicop_731'] + ['poste_coicop_732'] + ['poste_coicop_733'] +
-            ['poste_coicop_734'] + ['poste_coicop_736']]
+        df = aggregates_data_frame[['poste_coicop_722', 'coicop12_7', 'poste_coicop_711'] +
+            ['poste_coicop_712', 'poste_coicop_713', 'poste_coicop_721', 'poste_coicop_723'] +
+            ['poste_coicop_724', 'poste_coicop_731', 'poste_coicop_732', 'poste_coicop_733'] +
+            ['poste_coicop_734', 'poste_coicop_736']]
         df['check'] = (
             df['poste_coicop_711'] + df['poste_coicop_712'] + df['poste_coicop_713'] + df['poste_coicop_721'] +
             df['poste_coicop_722'] + df['poste_coicop_723'] + df['poste_coicop_724'] + df['poste_coicop_731'] +
@@ -26,10 +26,10 @@ for year in [2000, 2005, 2011]:
             df['coicop12_7']
             ).copy()
     else:
-        df = aggregates_data_frame[['poste_coicop_722'] + ['coicop12_7'] + ['poste_coicop_711'] +
-            ['poste_coicop_712'] + ['poste_coicop_735'] + ['poste_coicop_713'] + ['poste_coicop_721'] +
-            ['poste_coicop_723'] + ['poste_coicop_724'] + ['poste_coicop_731'] + ['poste_coicop_732'] +
-            ['poste_coicop_733'] + ['poste_coicop_734'] + ['poste_coicop_736']]
+        df = aggregates_data_frame[['poste_coicop_722', 'coicop12_7', 'poste_coicop_711'] +
+            ['poste_coicop_712', 'poste_coicop_735', 'poste_coicop_713', 'poste_coicop_721'] +
+            ['poste_coicop_723', 'poste_coicop_724', 'poste_coicop_731', 'poste_coicop_732'] +
+            ['poste_coicop_733', 'poste_coicop_734', 'poste_coicop_736']]
         df['check'] = (
             df['poste_coicop_711'] + df['poste_coicop_712'] + df['poste_coicop_713'] + df['poste_coicop_721'] +
             df['poste_coicop_722'] + df['poste_coicop_723'] + df['poste_coicop_724'] + df['poste_coicop_731'] +
@@ -38,6 +38,3 @@ for year in [2000, 2005, 2011]:
             ).copy()
     assert_near(df['check'].any(), 0, 0.0001), "the total of transport differs from the sum of its components"
 
-    df2 = aggregates_data_frame[['poste_coicop_722'] + ['categorie_fiscale_14']]
-    df2['check'] = df2['poste_coicop_722'] - df2['categorie_fiscale_14']
-    assert_near(df2['check'].any(), 0, 0.0001), "categorie_fiscale_14 is not correctly constructed"
