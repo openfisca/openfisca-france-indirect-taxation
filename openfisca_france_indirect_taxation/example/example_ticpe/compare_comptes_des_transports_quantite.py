968731d1c673dabc8832a3b55e034b9b3e631fa9
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-01-28 12:25:17 +0100

    Add seaborn to examples

--- openfisca_france_indirect_taxation/example/example_ticpe/compare_comptes_des_transports_quantite.py
+++ openfisca_france_indirect_taxation/example/example_ticpe/compare_comptes_des_transports_quantite.py
@@ -15,9 +15,13 @@ import pkg_resources
 import os
 import pandas as pd
 from pandas import concat
+import seaborn
 
 # Import de modules spécifiques à Openfisca
 from openfisca_france_indirect_taxation.example.utils_example import graph_builder_line
+
+# Import d'une nouvelle palette de couleurs
+seaborn.set_palette(seaborn.color_palette("Set2", 12))
 
 # Import des fichiers csv donnant les montants agrégés des quantités consommées répertoriées dans les enquêtes BdF.
 # Ces montants sont calculés dans compute_quantite_carburants
