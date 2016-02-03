471002d7f6b0708a79420c7961b445955de79db7
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-02-03 18:55:20 +0100

    Add one category of goods

--- openfisca_france_indirect_taxation/almost_ideal_demand_system/aids_estimation_from_stata.py
+++ openfisca_france_indirect_taxation/almost_ideal_demand_system/aids_estimation_from_stata.py
@@ -21,7 +21,8 @@ for year in [2000, 2005, 2011]:
         os.path.join(
             default_config_files_directory,
             'openfisca_france_indirect_taxation',
-            'almost_ideal_demand_system',
+            'assets',
+            'quaids',
             'data_quaids_{}.csv'.format(year)
             ), sep =',')
 
@@ -31,16 +32,16 @@ for year in [2000, 2005, 2011]:
     data_quaids.fillna(0, inplace=True)
     assert 0.999 < sum(data_quaids['part_depenses_tot']) < 1.001, "the sum of the shares is not equal to 1"
 
-    for i in range(1, 4):
+    for i in range(1, 5):
         data_quaids['el_{}'.format(i)] = \
             data_quaids['mu_{}'.format(i)] * data_quaids['part_depenses_tot']
 
     # Compute the estimation of the income elasticities of consumption
-    for i in range(1, 4):
+    for i in range(1, 5):
         resultats_elasticite_depenses['el_{0}_{1}'.format(i, year)] = sum(data_quaids['el_{}'.format(i)])
 
     # Compute the 95% confidence interval for those elasticities
-    for i in range(1, 4):
+    for i in range(1, 5):
         borne_superieure_el_dep['borne_sup_{0}_{1}'.format(i, year)] = (
             resultats_elasticite_depenses['el_{0}_{1}'.format(i, year)] + 1.96 *
             (data_quaids['mu_{}'.format(i)].describe()['std'] /
@@ -52,10 +53,10 @@ for year in [2000, 2005, 2011]:
             len(data_quaids['mu_{}'.format(i)]) ** 0.5)
             )
 
-    for i in range(1, 4):
+    for i in range(1, 5):
         data_quaids['el_uncomp_{}'.format(i)] = \
             data_quaids['ce_{}_{}'.format(i, i)] * data_quaids['part_depenses_tot']
 
-    # Compute the estimation of the income elasticities of consumption
-    for i in range(1, 4):
+    # Compute the estimation of the uncompensated price elasticities of consumption
+    for i in range(1, 5):
         resultats_elasticite_uncomp['el_uncomp_{0}_{1}'.format(i, year)] = sum(data_quaids['el_uncomp_{}'.format(i)])
