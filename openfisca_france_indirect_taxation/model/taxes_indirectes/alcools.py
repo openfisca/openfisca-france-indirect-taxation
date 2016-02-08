307ceb196ab278080f39f8aa27b2d0f194c900e3
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-02-08 14:12:42 +0100

    Clean alcools

--- openfisca_france_indirect_taxation/model/taxes_indirectes/alcools.py
+++ openfisca_france_indirect_taxation/model/taxes_indirectes/alcools.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 17 14:57:57 2015
 
-@author: thomas.douenne
-"""
 
 from __future__ import division
 
@@ -19,10 +15,9 @@ class alcools_forts_droit_d_accise(Variable):
     def function(self, simulation, period):
         consommation_alcools_forts = simulation.calculate('consommation_alcools_forts', period)
         taux_plein_tva = simulation.legislation_at(period.start).imposition_indirecte.tva.taux_plein
-        droit_cn = \
-            simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.alcools_forts.droit_cn_alcools_total
-        consommation_cn = \
-            simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.alcools_forts.masse_conso_cn_alcools
+        alcool_conso_et_vin = simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin
+        droit_cn = alcool_conso_et_vin.alcools_forts.droit_cn_alcools_total
+        consommation_cn = alcool_conso_et_vin.alcools_forts.masse_conso_cn_alcools
         return period, droit_d_accise(consommation_alcools_forts, droit_cn, consommation_cn, taux_plein_tva)
 
 
@@ -34,9 +29,9 @@ class biere_droit_d_accise(Variable):
     def function(self, simulation, period):
         consommation_biere = simulation.calculate('consommation_biere', period)
         taux_plein_tva = simulation.legislation_at(period.start).imposition_indirecte.tva.taux_plein
-        droit_cn = simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.biere.droit_cn_biere
-        consommation_cn = \
-            simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.biere.masse_conso_cn_biere
+        alcool_conso_et_vin = simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin
+        droit_cn = alcool_conso_et_vin.biere.droit_cn_biere
+        consommation_cn = alcool_conso_et_vin.biere.masse_conso_cn_biere
         return period, droit_d_accise(consommation_biere, droit_cn, consommation_cn, taux_plein_tva)
 
 
@@ -60,7 +55,7 @@ class vin_droit_d_accise(Variable):
     def function(self, simulation, period):
         consommation_vin = simulation.calculate('consommation_vin', period)
         taux_plein_tva = simulation.legislation_at(period.start).imposition_indirecte.tva.taux_plein
-        droit_cn = simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.vin.droit_cn_vin
-        consommation_cn = \
-            simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin.vin.masse_conso_cn_vin
+        alcool_conso_et_vin = simulation.legislation_at(period.start).imposition_indirecte.alcool_conso_et_vin
+        droit_cn = alcool_conso_et_vin.vin.droit_cn_vin
+        consommation_cn = alcool_conso_et_vin.vin.masse_conso_cn_vin
         return period, droit_d_accise(consommation_vin, droit_cn, consommation_cn, taux_plein_tva)
