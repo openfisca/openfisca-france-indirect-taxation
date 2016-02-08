a4b011521e41c8b0fbfb1d95027408d21c43a0f0
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-02-08 16:14:29 +0100

    Use inflators in example

--- openfisca_france_indirect_taxation/examples/all_consumptions/plot_consumption_distribution_1995_2011.py
+++ openfisca_france_indirect_taxation/examples/all_consumptions/plot_consumption_distribution_1995_2011.py
@@ -10,6 +10,7 @@ import seaborn
 # Import de modules spécifiques à Openfisca
 from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_bar
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
+from openfisca_france_indirect_taxation.examples.calage_bdf_cn_bis import get_inflators_by_year, get_aggregates_by_year
 
 # Import d'une nouvelle palette de couleurs
 seaborn.set_palette(seaborn.color_palette("Set2", 12))
@@ -17,10 +18,15 @@ seaborn.set_palette(seaborn.color_palette("Set2", 12))
 
 if __name__ == '__main__':
 
+
+    inflators_by_year = get_inflators_by_year()
     # Liste des coicop agrégées en 12 postes
     simulated_variables = ['coicop12_{}'.format(coicop12_index) for coicop12_index in range(1, 13)]
+
+
     for year in [2000, 2005, 2011]:
-        survey_scenario = SurveyScenario.create(year = year)
+        inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year])
+        survey_scenario = SurveyScenario.create(year = year, inflation_kwargs = inflation_kwargs)
         pivot_table = pandas.DataFrame()
         for values in simulated_variables:
             pivot_table = pandas.concat([
