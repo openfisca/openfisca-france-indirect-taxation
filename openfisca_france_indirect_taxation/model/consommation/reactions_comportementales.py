ddfe0e6ef85981b891f4a57f0f437e78a601e495
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-02-10 17:37:34 +0100

    Improve reaction comportementale and associated test

--- openfisca_france_indirect_taxation/model/consommation/reactions_comportementales.py
+++ openfisca_france_indirect_taxation/model/consommation/reactions_comportementales.py
@@ -16,28 +16,27 @@ class carburants_elasticite_prix(Variable):
     entity_class = Menages
     label = u"Elasticite prix carburants"
 
-    def function(self, simulation, period):
-        return period, carburants_elasticite_prix
-
 
 class essence_depenses_ajustees(Variable):
     column = FloatCol
     entity_class = Menages
-    label = "Depenses en essence après réaction à la réforme des prix"
+    label = u"Dépenses en essence après réaction à la réforme des prix"
 
     def function(self, simulation, period):
         essence_depenses = simulation.calculate('essence_depenses', period)
         super_95_ttc = simulation.legislation_at(period.start).imposition_indirecte.prix_carburants.super_95_ttc
-        reforme_essence = simulation.legislation_at(period.start).imposition_indirecte.prix_carburants.reforme_essence
+        reforme_essence = 30
+        # simulation.legislation_at(period.start).imposition_indirecte.prix_carburants.reforme_essence
         carburants_elasticite_prix = simulation.calculate('carburants_elasticite_prix')
-        essence_depenses_ajustees = essence_depenses * (1 + (carburants_elasticite_prix * reforme_essence / super_95_ttc))
+        essence_depenses_ajustees = \
+            essence_depenses * (1 + (carburants_elasticite_prix * reforme_essence / super_95_ttc))
         return period, essence_depenses_ajustees
 
 
 class diesel_depenses_ajustees(Variable):
     column = FloatCol
     entity_class = Menages
-    label = "Depenses en diesel après réaction à la réforme des prix"
+    label = u"Dépenses en diesel après réaction à la réforme des prix"
 
     def function(self, simulation, period):
         diesel_depenses = simulation.calculate('diesel_depenses', period)
