821153d5a07d864745ffe60ddfdbf5d5ad27ff69
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-02-12 19:49:34 +0100

    WIP

--- openfisca_france_indirect_taxation/scripts/build_coicop_bdf.py
+++ openfisca_france_indirect_taxation/scripts/build_coicop_bdf.py
@@ -8,6 +8,7 @@ from openfisca_survey_manager import default_config_files_directory as config_fi
 from openfisca_survey_manager.survey_collections import SurveyCollection
 
 from openfisca_france_indirect_taxation.scripts.build_coicop_nomenclature import build_coicop_nomenclature
+from openfisca_france_indirect_taxation.scripts.build_coicop_legislation import get_categorie_fiscale
 
 
 def coicop_from_aliss():
@@ -17,35 +18,44 @@ def coicop_from_aliss():
         )
     survey = aliss_survey_collection.get_survey('aliss_{}'.format(year))
 
-    df = survey.get_values(table = 'Base_ALISS_2011')
-    dirty_produits = df.souscode.unique()
+    aliss = survey.get_values(table = 'Base_ALISS_2011')
+    dirty_produits = aliss.souscode.unique()
     code_coicop_by_dirty_produits = dict()
 
     for dirty_produit in dirty_produits:
         code_coicop = '0' + '.'.join(dirty_produit[:4])
         code_coicop_by_dirty_produits[dirty_produit[6:]] = code_coicop
 
-    return pandas.DataFrame(dict(
+    result = pandas.DataFrame(dict(
         dirty = code_coicop_by_dirty_produits.keys(),
         code_coicop = code_coicop_by_dirty_produits.values()
         ))
+    assert not result.code_coicop.duplicated().any()
+
+    return result
 
 
 def coicop_from_bdf():
     year = 2011
-
     openfisca_survey_collection = SurveyCollection.load(collection = "openfisca_indirect_taxation")
     openfisca_survey = openfisca_survey_collection.get_survey("openfisca_indirect_taxation_data_{}".format(year))
     input_data_frame = openfisca_survey.get_values(table = "input")
     input_data_frame.reset_index(inplace = True)
-    [col for col in input_data_frame.columns if col.startswith('post')]
-    print l
+
+    postes_coicop = [col for col in input_data_frame.columns if col.startswith('post')]
+    postes_coicop
     dirty_produits = df.souscode.unique()
     code_coicop_by_dirty_produits = dict()
 
     for dirty_produit in dirty_produits:
         code_coicop = '0' + '.'.join(dirty_produit[:4])
         code_coicop_by_dirty_produits[dirty_produit[6:]] = code_coicop
+
+    result = pandas.DataFrame(dict(
+        dirty = code_coicop_by_dirty_produits.keys(),
+        code_coicop = code_coicop_by_dirty_produits.values()
+        ))
+    return result
 
 
 def merge_with_coicop(data_frame):
@@ -60,8 +70,30 @@ def merge_with_coicop(data_frame):
         ]].sort_values(by = u'poste_coicop')
 
 
-data_frame = coicop_from_aliss()
-df = merge_with_coicop(data_frame)
+def test_coicop_to_legislation(data_frame, adjust_coicop):
+    for code_coicop in data_frame.code_coicop.unique():
+        selection = data_frame.loc[data_frame.code_coicop == code_coicop].copy()
+        try:
+            print code_coicop, selection.dirty.unique()
+            print get_categorie_fiscale(
+                adjust_coicop.get(code_coicop, code_coicop), year = 2010)
+        except:
+            print selection
+            raise()
+
+def aliss():
+    adjust_coicop = {
+        '01.1.5.2': '01.1.5.2.2',
+        '01.1.8.1': '01.1.8.1.1',
+        }
+    aliss_coicop = coicop_from_aliss()
+    data_frame = merge_with_coicop(aliss_coicop)
+    test_coicop_to_legislation(data_frame, adjust_coicop)
+
+
+def bdf():
+    bdf_coicop = coicop_from_bdf()
+    data_frame = merge_with_coicop(aliss_coicop)
 
 
 # TODO check notamment problème avec sucre confiseries
