0218fce14d4df5f09811eb5a03545af7d62d8128
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-02-29 14:27:56 +0100

    Add new variables into the model for elasticities

--- openfisca_france_indirect_taxation/model/consommation/elasticites.py
+++ openfisca_france_indirect_taxation/model/consommation/elasticites.py
@@ -11,3 +11,117 @@ class elas_exp_1(Variable):
     column = FloatCol(default = 0)  # FloatCol(default = 0)
     entity_class = Menages
     label = u"Elasticité dépense carburants"
+
+
+class elas_exp_2(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité dépense énergie logement"
+
+
+class elas_exp_3(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité dépense alimentaire"
+
+
+class elas_exp_4(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité dépense autres biens non durables"
+
+
+class elas_price_1_1(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix carburants"
+
+
+class elas_price_1_2(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée carburants - énergie logement"
+
+
+class elas_price_1_3(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée carburants - alimentaire"
+
+
+class elas_price_1_4(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée carburants - autres biens non durables"
+
+
+class elas_price_2_1(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée énergie logement - carburants"
+
+
+class elas_price_2_2(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix énergie logement"
+
+
+class elas_price_2_3(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée énergie logement - alimentaire"
+
+
+class elas_price_2_4(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée énergie logement - autres biens non durables"
+
+
+class elas_price_3_1(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée alimentaire - carburants"
+
+
+class elas_price_3_2(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée énergie logement - alimentaire"
+
+
+class elas_price_3_3(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix énergie logement"
+
+
+class elas_price_3_4(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée énergie logement - autres biens non durables"
+
+
+class elas_price_4_1(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée autres biens non durables - carburants"
+
+
+class elas_price_4_2(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée autres biens non durables - énergie logement"
+
+
+class elas_price_4_3(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix croisée autres biens non durables - alimentaire"
+
+
+class elas_price_4_4(Variable):
+    column = FloatCol(default = 0)  # FloatCol(default = 0)
+    entity_class = Menages
+    label = u"Elasticité prix autres biens non durables"
