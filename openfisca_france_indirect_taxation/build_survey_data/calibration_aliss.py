551481543b8bc1124ac0d2ed3f9139d29e70f370
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-03-03 12:21:02 +0100

    WIP aliss

--- openfisca_france_indirect_taxation/build_survey_data/calibration_aliss.py
+++ openfisca_france_indirect_taxation/build_survey_data/calibration_aliss.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import division
+
+
+import itertools
+import pandas
+
 
 from openfisca_survey_manager.survey_collections import SurveyCollection
 from openfisca_survey_manager import default_config_files_directory as config_files_directory
 
 
-def coicop_from_aliss():
+def build_clean_aliss_data_frame():
     year = 2011
     aliss_survey_collection = SurveyCollection.load(
         collection = 'aliss', config_files_directory = config_files_directory
@@ -13,9 +19,69 @@ def coicop_from_aliss():
     survey = aliss_survey_collection.get_survey('aliss_{}'.format(year))
 
     aliss = survey.get_values(table = 'Base_ALISS_2011')
+    aliss['age'] = 99
+    aliss['revenus'] = 99
 
-    by = ['souscode', 'clas', 'zeatinse', 'age']
-    grouped = aliss.groupby(by = by)
+    triplets = [
+        ('1 : Jeune/Ais', 0, 3),
+        ('2 : Jeune/MoyenSup', 0, 2),
+        ('3 : Jeune/MoyenInf', 0, 1),
+        ('4 : Jeune/Modeste', 0, 0),
+        ('5 : Age Moyen/Ais', 1, 3),
+        ('6 : Age Moyen/MoyenSup', 1, 2),
+        ('7 : Age Moyen/MoyenInf', 1, 1),
+        ('8 : Age Moyen/Modeste', 1, 0),
+        ('9 : Age Sup/Ais', 2, 3),
+        ('10 : Age Sup/MoyenSup', 2, 2),
+        ('11 : Age Sup/MoyenInf', 2, 1),
+        ('12 : Age Sup/Modeste', 2, 0),
+        ('13 : Vieux/Ais', 3, 3),
+        ('14 : Vieux/MoyenSup', 3, 2),
+        ('15 : Vieux/MoyenInf', 3, 1),
+        ('16 : Vieux/Modeste', 3, 0),
+        ]
+
+    for household_type, age, revenus in triplets:
+        print household_type, age, revenus
+        selection = aliss.type.str.startswith(household_type)
+        aliss.loc[selection, 'age'] = age
+        aliss.loc[selection, 'revenus'] = revenus
+
+    assert aliss.age.isin(range(4)).all()
+    assert aliss.revenus.isin(range(4)).all()
+    del aliss['type']
+
+    return aliss
 
 
+def compute_correction_coefficient():
+    # Calculer les cales
+    pass
 
+
+def compute_kantar_elasticities():
+    kantar_budget_share = pandas.DataFrame()
+    for age, revenus, nomf in itertools.product(aliss.age.unique(), aliss.revenus.unique(), aliss.nomf.unique()):
+
+        print '\n', age, revenus, nomf
+
+        extract = aliss.query(
+            'nomf == @nomf & age == @age & revenus == @revenus'
+            )[
+                ['age', 'revenus', 'nomk', 'dm_k', 'dm_f']
+            ]
+
+        assert len(extract.dm_f.unique()) == 1
+        extract['budget_share_kf'] = extract.dm_k / extract.dm_f
+        kantar_budget_share = kantar_budget_share.append(extract)
+        print len(kantar_elasticities)
+
+    for age, revenus in itertools.product(aliss.age.unique(), aliss.revenus.unique()):
+        print age, revenus
+
+    return kantar_budget_share
+
+
+if __name__ == '__main__':
+    aliss = build_clean_aliss_data_frame()
+    kantar_elasticities = compute_kantar_elasticities()
--- openfisca_france_indirect_taxation/scripts/read_aliss_elasticites.py
+++ openfisca_france_indirect_taxation/scripts/read_aliss_elasticites.py
@@ -22,108 +22,114 @@ elasticities_origin_xlsx = os.path.join(
 
 tables = [
     dict(
+        age = None,
+        revenus = None,
         skiprows = 4,
         sheetname = 'B.3',
         parse_cols = "C:T",
         name = 'Food Expenditure Elasticities'
         ),
     dict(
+        age = None,
+        revenus = None,
         skiprows = 6,
         sheetname = 'B.4',
         parse_cols = "B:Y",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Full sample'
         ),
     dict(
+        age = 0,
+        revenus = 3,
         skiprows = 5,
         sheetname = 'B5',
         parse_cols = "B:Y",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Well off and less than 30 years old'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B6',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Well off and aged between 30 and 45'
         ),
-     dict(
+    dict(
         skiprows = 5,
         sheetname = 'B7',
         parse_cols = "B:Y",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Well off and aged between 45 and 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B8',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Well off and more than 60'
         ),
-     dict(
+    dict(
         skiprows = 5,
         sheetname = 'B9',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Upper average and less than 30 years old'
         ),
-     dict(
+    dict(
         skiprows = 5,
         sheetname = 'B10',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Upper average and aged between 30 and 45'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B11',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Upper average and aged between 45 and 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B12',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Upper average and aged more than 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B13',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Lower average and less than 30 years old'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B14',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Lower average and aged between 30 and 45'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B15',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Lower average and aged between 45 and 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B16',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Lower average and aged more than 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B17',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Modest and less than 30 years old'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B18',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Modest and aged between 30 and 45'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B19',
         parse_cols = "C:Z",
         name = 'Compensated Own-Price and Cross-Price Elasticities -- Modest and aged between 45 and 60'
         ),
-     dict(
+    dict(
         skiprows = 4,
         sheetname = 'B20',
         parse_cols = "C:Z",
@@ -131,15 +137,27 @@ tables = [
         ),
     ]
 
+cross_price_elasticities = pandas.DataFrame()
 for table in tables:
+    age = table.pop('age')
     name = table.pop('name')
+    revenus = table.pop('revenus')
+    if age is None or revenus is None:
+        # save elsewhere
+        continue
+
     df = pandas.read_excel(elasticities_origin_xlsx, **table)
     df.dropna(inplace = True)
     df.set_index('Unnamed: 0', inplace = True)
     df.index.name = 'product'
     df.name = name
-    csv_path_name = os.path.join(
-        elasticities_path,
-        slugify(name) + '.csv',
-        )
-    df.to_csv(csv_path_name)
+    df['age'] = age
+    df['revenus'] = revenus
+    cross_price_elasticities = cross_price_elasticities.append(df)
+
+
+csv_path_name = os.path.join(
+    elasticities_path,
+    'cross_price_elasticities.csv',
+    )
+cross_price_elasticities.to_csv(csv_path_name)
