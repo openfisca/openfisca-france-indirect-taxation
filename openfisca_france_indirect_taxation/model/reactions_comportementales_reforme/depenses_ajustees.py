1df13c9311c443e7816cf8a6aad6b2191c328e1f
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-03-18 11:11:24 +0100

    Create new variable for adjusted expenditure of gaz

--- openfisca_france_indirect_taxation/model/reactions_comportementales_reforme/depenses_ajustees.py
+++ openfisca_france_indirect_taxation/model/reactions_comportementales_reforme/depenses_ajustees.py
@@ -19,6 +19,7 @@ class depenses_essence_ajustees(Variable):
         carburants_elasticite_prix = simulation.calculate('elas_price_1_1')
         depenses_essence_ajustees = \
             depenses_essence * (1 + (1 + carburants_elasticite_prix) * reforme_essence / super_95_ttc)
+
         return period, depenses_essence_ajustees
 
 
@@ -32,6 +33,25 @@ class depenses_diesel_ajustees(Variable):
         diesel_ttc = simulation.legislation_at(period.start).imposition_indirecte.prix_carburants.diesel_ttc
         reforme_diesel = simulation.legislation_at(period.start).taxes_carburants.diesel
         carburants_elasticite_prix = simulation.calculate('elas_price_1_1')
-        depenses_essence_ajustees = \
+        depenses_diesel_ajustees = \
             depenses_diesel * (1 + (1 + carburants_elasticite_prix) * reforme_diesel / diesel_ttc)
-        return period, depenses_essence_ajustees
+
+        return period, depenses_diesel_ajustees
+
+
+class depenses_gaz_ajustees_taxe_carbone(Variable):
+    column = FloatCol
+    entity_class = Menages
+    label = u"Dépenses en gaz après réaction à la réforme - taxe carbone"
+
+    def function(self, simulation, period):
+        depenses_gaz_variables = simulation.calculate('depenses_gaz_variables', period)
+        depenses_gaz_prix_unitaire = simulation.calculate('depenses_gaz_prix_unitaire', period)
+        reforme_gaz = simulation.legislation_at(period.start).taxe_carbone.gaz
+        gaz_elasticite_prix = simulation.calculate('elas_price_2_2')
+        depenses_gaz_ajustees_variable = \
+            depenses_gaz_variables * (1 + (1 + gaz_elasticite_prix) * reforme_gaz / depenses_gaz_prix_unitaire)
+        depenses_gaz_tarif_fixe = simulation.calculate('depenses_gaz_tarif_fixe', period)
+        depenses_gaz_ajustees = depenses_gaz_ajustees_variable + depenses_gaz_tarif_fixe
+
+        return period, depenses_gaz_ajustees
