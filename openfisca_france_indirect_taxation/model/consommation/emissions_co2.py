519f594b41d0395a9720dcc7e35f0754fb09046e
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-03-18 14:57:41 +0100

    Compute CO2 emissions from electricity consumption

--- openfisca_france_indirect_taxation/model/consommation/emissions_co2.py
+++ openfisca_france_indirect_taxation/model/consommation/emissions_co2.py
@@ -35,3 +35,17 @@ class emissions_CO2_gaz(Variable):
         emissions = quantites_gaz * emissions_gaz
 
         return period, emissions
+
+
+class emissions_CO2_electricite(Variable):
+    column = FloatCol
+    entity_class = Menages
+    label = u"Emissions de CO2 des ménages via leur consommation d'électricité, en kg de CO2"
+
+    def function(self, simulation, period):
+        quantites_eletricite = simulation.calculate('quantites_electricite_selon_compteur', period)
+        emissions_electricite = \
+            simulation.legislation_at(period.start).imposition_indirecte.emissions_CO2.energie_logement.CO2_electricite
+        emissions = quantites_eletricite * emissions_electricite
+
+        return period, emissions
