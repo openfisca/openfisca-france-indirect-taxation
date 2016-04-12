49e023125224e314a7510573d4e5753eb2c16a0a
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-04-12 17:01:23 +0200

    Add total emissions and reorganize module

--- openfisca_france_indirect_taxation/model/consommation/emissions_co2.py
+++ openfisca_france_indirect_taxation/model/consommation/emissions_co2.py
@@ -23,6 +23,53 @@ class emissions_CO2_carburants(Variable):
         return period, emissions
 
 
+class emissions_CO2_electricite(Variable):
+    column = FloatCol
+    entity_class = Menages
+    label = u"Emissions de CO2 des ménages via leur consommation d'électricité, en kg de CO2"
+
+    def function(self, simulation, period):
+        quantites_eletricite = simulation.calculate('quantites_electricite_selon_compteur', period)
+        emissions_electricite = \
+            simulation.legislation_at(period.start).imposition_indirecte.emissions_CO2.energie_logement.CO2_electricite
+        emissions = quantites_eletricite * emissions_electricite
+
+        return period, emissions
+
+
+class emissions_CO2_energies(Variable):
+    column = FloatCol
+    entity_class = Menages
+    label = u"Emissions de CO2 des ménages via leur consommation d'énergies (sans combustibles solides), en kg de CO2"
+
+    def function(self, simulation, period):
+        emissions_carburants = simulation.calculate('emissions_CO2_carburants', period)
+        emissions_electricite = simulation.calculate('emissions_CO2_electricite', period)
+        emissions_fioul_domestique = simulation.calculate('emissions_CO2_fioul_domestique', period)
+        emissions_gaz = simulation.calculate('emissions_CO2_gaz', period)
+
+        emissions_energies = (
+            emissions_carburants + emissions_electricite + emissions_fioul_domestique + emissions_gaz
+            )
+        return period, emissions_energies
+
+
+class emissions_CO2_energies_logement(Variable):
+    column = FloatCol
+    entity_class = Menages
+    label = u"Emissions de CO2 des ménages via leur consommation d'énergies pour le logement, en kg de CO2"
+
+    def function(self, simulation, period):
+        emissions_electricite = simulation.calculate('emissions_CO2_electricite', period)
+        emissions_fioul_domestique = simulation.calculate('emissions_CO2_fioul_domestique', period)
+        emissions_gaz = simulation.calculate('emissions_CO2_gaz', period)
+
+        emissions_energies = (
+            emissions_electricite + emissions_fioul_domestique + emissions_gaz
+            )
+        return period, emissions_energies
+
+
 class emissions_CO2_fioul_domestique(Variable):
     column = FloatCol
     entity_class = Menages
@@ -47,19 +94,5 @@ class emissions_CO2_gaz(Variable):
         emissions_gaz = \
             simulation.legislation_at(period.start).imposition_indirecte.emissions_CO2.energie_logement.CO2_gaz
         emissions = quantites_gaz * emissions_gaz
-
-        return period, emissions
-
-
-class emissions_CO2_electricite(Variable):
-    column = FloatCol
-    entity_class = Menages
-    label = u"Emissions de CO2 des ménages via leur consommation d'électricité, en kg de CO2"
-
-    def function(self, simulation, period):
-        quantites_eletricite = simulation.calculate('quantites_electricite_selon_compteur', period)
-        emissions_electricite = \
-            simulation.legislation_at(period.start).imposition_indirecte.emissions_CO2.energie_logement.CO2_electricite
-        emissions = quantites_eletricite * emissions_electricite
 
         return period, emissions
