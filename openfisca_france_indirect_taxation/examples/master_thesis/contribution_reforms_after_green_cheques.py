6217d7e01131e69b1a0b8f36d8d3960e64cd1311
thomasdouenne <thomas.douenne@sciencespo.fr>
2016-04-22 18:38:20 +0200

    Clean what is useless

--- openfisca_france_indirect_taxation/examples/master_thesis/contribution_reforms_after_green_cheques.py
+++ openfisca_france_indirect_taxation/examples/master_thesis/contribution_reforms_after_green_cheques.py
@@ -6,7 +6,7 @@ from __future__ import division
 import pandas
 
 # Import modules specific to OpenFisca
-from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_line_percent, graph_builder_bar, \
+from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_bar, \
     save_dataframe_to_graph
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.almost_ideal_demand_system.aids_estimation_from_stata import get_elasticities
@@ -52,32 +52,22 @@ for reforme in ['taxes_carburants', 'taxe_carbone', 'cce_2014_2015', 'cce_2014_2
     contribution = (menages['difference_contribution_energie_{}'.format(reforme)] * menages['pondmen']).sum()
     contribution_unite_conso = contribution / unite_conso
 
-    #for category in ['niveau_vie_decile', 'age_group_pr', 'strate_agrege']:
-    pivot_table = pandas.DataFrame()
-    for values in simulated_variables:
-        pivot_table = pandas.concat([
-            pivot_table,
-            survey_scenario.compute_pivot_table(values = [values], columns = ['niveau_vie_decile'])
-            ])
-    df = pivot_table.T
-    df['Additional contribution on TICPE - energy expenditures'] = (
-        (df['difference_contribution_energie_{}'.format(reforme)] - (contribution_unite_conso) * df['ocde10']) /
-        df['depenses_energies']
-        )
-    df['Additional effort rate on TICPE reform - income'] = (
-        (df['difference_contribution_energie_{}'.format(reforme)] - (contribution_unite_conso) * df['ocde10']) /
-        df['rev_disp_loyerimput']
-        )
-    df.rename(
-        columns = {'difference_contribution_energie_{}'.format(reforme): 'Cost of the reform born by households'},
-        inplace = True)
-    save_dataframe_to_graph(
-        df, 'Contributions_reforme/contribution_apres_cheques_verts_reforme.csv'
-        )
+    for category in ['niveau_vie_decile', 'age_group_pr', 'strate_agrege']:
+        pivot_table = pandas.DataFrame()
+        for values in simulated_variables:
+            pivot_table = pandas.concat([
+                pivot_table,
+                survey_scenario.compute_pivot_table(values = [values], columns = ['{}'.format(category)])
+                ])
+        df = pivot_table.T
+        df[u'Cost of the reform after green cheques'] = (
+            ((contribution_unite_conso) * df['ocde10'] - df['difference_contribution_energie_{}'.format(reforme)])
+            )
 
-    # Réalisation de graphiques
-    graph_builder_bar(df['Cost of the reform born by households'] - (contribution_unite_conso) * df['ocde10'])
-    graph_builder_line_percent(
-        df[['Additional contribution on TICPE - energy expenditures',
-        'Additional effort rate on TICPE reform - income']]
-        )
+        save_dataframe_to_graph(
+            df[u'Cost of the reform after green cheques'],
+            'Contributions_reforme/contribution_{0}_apres_cheques_verts_by_{1}.csv'.format(reforme, category)
+            )
+
+        # Réalisation de graphiques
+        graph_builder_bar(df[u'Cost of the reform after green cheques'])
