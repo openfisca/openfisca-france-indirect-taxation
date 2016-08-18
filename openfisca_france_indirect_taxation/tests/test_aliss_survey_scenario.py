6840d00b128f11a1ab6ee95afd8656239af26cf4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-08-18 17:58:49 +0200

    Handle missing seaborn import to pass travis tests

--- openfisca_france_indirect_taxation/tests/test_aliss_survey_scenario.py
+++ openfisca_france_indirect_taxation/tests/test_aliss_survey_scenario.py
@@ -3,15 +3,18 @@
 
 from __future__ import division
 
+try:
+    import seaborn
+except ImportError:
+    seaborn = None
 
-import seaborn
 
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.build_survey_data.calibration_aliss import get_adjusted_input_data_frame
 
 # Import d'une nouvelle palette de couleurs
-seaborn.set_palette(seaborn.color_palette("Set2", 12))
-
+if import is not None:
+    seaborn.set_palette(seaborn.color_palette("Set2", 12))
 
 
 def build_aliss_scenarios(reform_key = None):
@@ -32,7 +35,6 @@ def build_aliss_scenarios(reform_key = None):
 def test():
     reform_key = "aliss_tva_sociale"
     survey_scenario, adjusted_survey_scenario = build_aliss_scenarios(reform_key = reform_key)
-
 
 
 if __name__ == '__main__':
