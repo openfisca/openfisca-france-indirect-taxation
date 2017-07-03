68e519d1a7d7c234d07191be1635c4f0260a2835
Thomas DOUENNE <t.douenne@pse.local>
2017-07-03 11:20:03 +0200

    Work on matching (WIP)

--- openfisca_france_indirect_taxation/build_survey_data/homogeneisation_bdf_enl/step_4_matching.py
+++ openfisca_france_indirect_taxation/build_survey_data/homogeneisation_bdf_enl/step_4_matching.py
@@ -6,8 +6,13 @@ from __future__ import division
 # We compare distribution of variables in the two surveys and assess if they need a correction or not to be homogenous
 
 from openfisca_france_indirect_taxation.build_survey_data.homogeneisation_bdf_enl.step_2_homogenize_variables import \
-    data_bdf, data_enl
+    homogenize_variables_definition_bdf_enl
 
 from openfisca_survey_manager.matching import nnd_hotdeck_using_rpy2
+
+data = homogenize_variables_definition_bdf_enl()    
+data_enl = data[0]
+data_bdf = data[1]
+
 
 nnd_hotdeck_using_rpy2(receiver = data_enl, donor = data_bdf, matching_variables = 'revtot')
