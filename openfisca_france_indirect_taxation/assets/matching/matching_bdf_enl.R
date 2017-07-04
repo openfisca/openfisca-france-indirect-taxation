d9f54270430736a37db3baee4fbf567df7b7e10a
thomasdouenne <thomas.douenne@sciencespo.fr>
2017-07-04 17:49:37 +0200

    Hide csv files for matching

--- openfisca_france_indirect_taxation/assets/matching/matching_bdf_enl.R
+++ openfisca_france_indirect_taxation/assets/matching/matching_bdf_enl.R
@@ -1,26 +1,34 @@
 # Import data
-data_enl <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matching_enl_small.csv", header = -1, sep=",")
-data_bdf <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matching_bdf_small.csv", header = -1, sep=",")
+data_enl <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matching_enl.csv", header = -1, sep=",")
+data_bdf <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matching_bdf.csv", header = -1, sep=",")
 
 # Compute matching
 out.nnd <- NND.hotdeck(
   data.rec = data_bdf, data.don = data_enl,
   match.vars = c("surfhab_d", "nbphab", "ocde10", "revtot", "poste_coicop_451",
                  "poste_coicop_452", "poste_coicop_453", "agepr", "aba",
-                 "cataeu", "tau", "tuu", "zeat")
+                 "cataeu", "tau", "tuu", "zeat"),
+  don.class = c("decile", "tuu")
 )
+
+# Compute matching - rank method
+
+# Compute matching - random method
+
+# Compute matching - mixed method
 
 # Create fused file
 fused.nnd.m <- create.fused(
   data.rec = data_bdf, data.don = data_enl,
   mtc.ids = out.nnd$mtc.ids,
-  z.vars = c("gchauf_1", "gchauf_2", "gchauf_3", "gchauf_4",
+  z.vars = c("gchauf_n", "gchauf_1", "gchauf_2", "gchauf_3", "gchauf_4",
              "gchauf_5", "gchauf_6", "gchauf_7", "gchaufs_1",
-             "gchaufs_2", "gchaufs_3", "gchaufs_4", "gchaufs_4",
+             "gchaufs_2", "gchaufs_3", "gchaufs_4", "gchaufs_5",
              "gmur", "gtoit2",
-             "part_energies_revenu")
+             "part_energies_revtot_after")
 )
 
 # Save it as csv
 write.csv(fused.nnd.m,
-          file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matched_small.csv")
+          file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/data_matched.csv"
+          )
