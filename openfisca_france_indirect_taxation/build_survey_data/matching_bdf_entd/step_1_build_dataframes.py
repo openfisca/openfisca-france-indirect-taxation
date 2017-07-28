9b9cbd0cc0359ceaa4e5c249c0992b0c7cb681cc
Thomas DOUENNE <t.douenne@pse.local>
2017-07-28 11:23:05 +0200

    Add variables to match ENTD

--- openfisca_france_indirect_taxation/build_survey_data/matching_bdf_entd/step_1_build_dataframes.py
+++ openfisca_france_indirect_taxation/build_survey_data/matching_bdf_entd/step_1_build_dataframes.py
@@ -27,7 +27,8 @@ def load_data_bdf_entd():
         )
     survey_entd = entd_survey_collection.get_survey('enquete_transport_{}'.format(year_entd))
     
-    input_entd_menages = survey_entd.get_values(table = 'q_menage')
+    input_entd_menages = survey_entd.get_values(table = 'q_tcm_menage')
+    input_entd_usage_veh = survey_entd.get_values(table = 'qf_voitvul')
     
     
     # Load BdF data :
@@ -63,15 +64,105 @@ def load_data_bdf_entd():
         # To be completed
         ]
     
-    variables_menages_entd = [
-        'ident_men'
+    variables_tcm_menages_entd = [
+        'ident_men',
+        'rg', # region de residence
+        'tu99',
+        'tau99',
+        'zus', # logement en zus
+        'typlog',
+        'axe2', #activite/chômage
+        'agpr',
+        'cs42pr',
+        'dip14pr',
+        'nactifs',
+        'nenfants',
+        'npers',
+        'revuc', # revenus simulés par UC
+        'nbuc', #ocde10
+        'nivie10', # déciles de revenu par uc
+        'rlog', # allocations logement
+        'situapr',
+        'typmen5', #type ménage
+        'pondv1', #poids ménage
+        'numcom_au2010', #cataeu
         # To be completed
         ]
 
+    variables_menages_entd = [
+        'ident_men',
+        'pondv1',
+        'v1_logdist01', #distance commerces et supermarchés
+        'v1_logdist15', # distance arrêts transports en commun
+        'v1_logpiec', # nombre de pièces dans logement
+        'v1_logocc', # statut d'occupation du logement
+        'v1_logloymens', # loyer mensuel calculé
+        'v1_jnbveh', # nb de voitures particulières
+        'v1_jnbmoto',
+        'v1_jnbcyclo',
+        'v1_jpasvoitb', # questions sur l'utilité du VP
+        ]
+
+    variables_qr_voitul_entd = [
+        'ident_men',
+        'mveh', # numéro du véhicule
+        'v1_ken', # type de carburant majoritairement utilisé
+        'v1_puiss_corr', # puissance fiscale corrigée
+        'v1_rang_veh', # place du veh dans le ménage
+        ]
+
+    variables_qf_voitvul_entd = [
+        'ident_men',
+        'poids_veh1', # poids du véhicule
+        'mveh', # numéro du véhicule
+        'v1_ken', # type de carburant majoritairement utilisé
+        'v1_puiss_corr', # puissance fiscale corrigée
+        'v1_age_veh',
+        'v1_rang_veh', # place du veh dans le ménage
+        'v1_sfvcosorout', # consommation du veh
+        'v1_sfvkm', # nombre de km au compteur aujourd'hui
+        'v1_km_annu', # nombre de km sur les 12 derniers mois redressés
+        ]
+
+    variables_q_individu_entd = [
+        'ident_men',
+        'noi', # numéro de l'individu dans le logement
+        'datenq',
+        'v1_gpermis', # X possède le permis B
+        ]
+
+    variables_q_ind_lieu_teg_entd = [
+        'ident_men',
+        'noi',
+        'v1_btravdist', # distance parcourue jusqu'au lieu de TEG
+        'v1_btravnbarj', # nb de déplacement par jour
+        ]
+        
+    variables_k_deploc_entd = [
+        'ident_men',
+        'datenq',
+        'pondki', #poids individu kish
+        'v2_mtp', # mode principal du déplacement,
+        'v2_midsttot', # distance du déplacement
+        ]
+
+    variables_c_dep_carnet_entd = [
+        'ident_men',
+        'poids_car1', # poids carnet
+        'danteq', # date de la visite 1
+        'distance', # distance parcourue
+        ]
 
     # Keep relevant variables :
-    menages_entd_keep = input_entd_menages[variables_menages_entd]
+    menages_entd_keep = input_entd_menages[variables_tcm_menages_entd]
+    usage_veh_entd_keep = input_entd_usage_veh[variables_qf_voitvul_entd]
     menages_bdf_keep = input_bdf[variables_menages_bdf]
+    
+    indiv_enl_keep = indiv_enl_keep.query('igreflog == 1')
+    del indiv_enl_keep['igreflog']
+    menages_entd_keep_bis = menages_entd_keep.merge(usage_veh_entd_keep, on = 'ident_men')
+
+
     del input_entd_menages, input_bdf
         
     return menages_entd_keep, menages_bdf_keep
