aea8f57a120727e75ca348086ed04c9af3ea3e9c
Thomas DOUENNE <t.douenne@pse.local>
2017-08-02 11:47:43 +0200

    Improve histograms to test matched data ENTD

--- openfisca_france_indirect_taxation/build_survey_data/matching_bdf_entd/step_5_3_histograms.py
+++ openfisca_france_indirect_taxation/build_survey_data/matching_bdf_entd/step_5_3_histograms.py
@@ -70,6 +70,71 @@ def histogrammes(list_keys, list_values_bdf, list_values_entd):
     return plt
 
 
+def plots_by_decile(function, data_matched):    
+    fig = plt.figure()
+    
+    ax1 = fig.add_subplot(521)
+    ax1 = function(data_matched, data_entd, 1)
+    
+    ax2 = fig.add_subplot(522)
+    ax2 = function(data_matched, data_entd, 2)
+    
+    ax3 = fig.add_subplot(523)
+    ax3 = function(data_matched, data_entd, 3)
+    
+    ax4 = fig.add_subplot(524)
+    ax4 = function(data_matched, data_entd, 4)
+
+    ax5 = fig.add_subplot(525)
+    ax5 = function(data_matched, data_entd, 5)
+    
+    ax6 = fig.add_subplot(526)
+    ax6 = function(data_matched, data_entd, 6)
+
+    ax7 = fig.add_subplot(527)
+    ax7 = function(data_matched, data_entd, 7)
+
+    ax8 = fig.add_subplot(528)
+    ax8 = function(data_matched, data_entd, 8)
+
+    ax9 = fig.add_subplot(529)
+    ax9 = function(data_matched, data_entd, 9)
+    
+    ax10 = fig.add_subplot(5,2,10)
+    ax10 = function(data_matched, data_entd, 10)
+
+
+def plots_by_tuu(function, data_matched):    
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(521)
+    ax1 = function(data_matched, data_entd, 0)
+    
+    ax2 = fig.add_subplot(522)
+    ax2 = function(data_matched, data_entd, 1)
+    
+    ax3 = fig.add_subplot(523)
+    ax3 = function(data_matched, data_entd, 2)
+    
+    ax4 = fig.add_subplot(524)
+    ax4 = function(data_matched, data_entd, 3)
+
+    ax5 = fig.add_subplot(525)
+    ax5 = function(data_matched, data_entd, 4)
+    
+    ax6 = fig.add_subplot(526)
+    ax6 = function(data_matched, data_entd, 5)
+
+    ax7 = fig.add_subplot(527)
+    ax7 = function(data_matched, data_entd, 6)
+
+    ax8 = fig.add_subplot(528)
+    ax8 = function(data_matched, data_entd, 7)
+
+    ax9 = fig.add_subplot(529)
+    ax9 = function(data_matched, data_entd, 8)
+
+
 def histogram_distance_annuelle(data_matched, data_entd):
     list_values_matched = []
     list_values_entd = []
@@ -109,138 +174,258 @@ def histogram_distance_annuelle(data_matched, data_entd):
     
     return plt
 
+
+def histogram_distance_diesel_annuelle(data_matched, data_entd):
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+
+    data_matched['distance_diesel'] = data_matched['distance_diesel'].astype(float)
+    data_matched['distance_diesel_racine'] = (data_matched['distance_diesel']) ** (0.5)
+    distance_diesel_racine_max_matched = data_matched['distance_diesel_racine'].max()
+
+    data_entd['distance_diesel'] = data_entd['distance_diesel'].astype(float)
+    data_entd['distance_diesel_racine'] = (data_entd['distance_diesel']) ** (0.5)
+    distance_diesel_racine_max_entd = data_entd['distance_diesel_racine'].max()
+
+    distance_diesel_racine_max = max(distance_diesel_racine_max_matched, distance_diesel_racine_max_entd)
+    data_matched['distance_diesel_groupe'] = (data_matched['distance_diesel_racine'] / distance_diesel_racine_max).round(decimals = 1)
+    data_entd['distance_diesel_groupe'] = (data_entd['distance_diesel_racine'] / distance_diesel_racine_max).round(decimals = 1)
+
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched.query('distance_diesel_groupe == {}'.format(j))['pondmen']) /
+            data_matched['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
     
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd.query('distance_diesel_groupe == {}'.format(j))['pondmen']) /
+            data_entd['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    histogrammes(list_keys, list_values_matched, list_values_entd)
+    
+    return plt
+    
+
+def histogram_distance_essence_annuelle(data_matched, data_entd):
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+
+    data_matched['distance_essence'] = data_matched['distance_essence'].astype(float)
+    data_matched['distance_essence_racine'] = (data_matched['distance_essence']) ** (0.5)
+    distance_essence_racine_max_matched = data_matched['distance_essence_racine'].max()
+
+    data_entd['distance_essence'] = data_entd['distance_essence'].astype(float)
+    data_entd['distance_essence_racine'] = (data_entd['distance_essence']) ** (0.5)
+    distance_essence_racine_max_entd = data_entd['distance_essence_racine'].max()
+
+    distance_essence_racine_max = max(distance_essence_racine_max_matched, distance_essence_racine_max_entd)
+    data_matched['distance_essence_groupe'] = (data_matched['distance_essence_racine'] / distance_essence_racine_max).round(decimals = 1)
+    data_entd['distance_essence_groupe'] = (data_entd['distance_essence_racine'] / distance_essence_racine_max).round(decimals = 1)
+
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched.query('distance_essence_groupe == {}'.format(j))['pondmen']) /
+            data_matched['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
+    
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd.query('distance_essence_groupe == {}'.format(j))['pondmen']) /
+            data_entd['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    histogrammes(list_keys, list_values_matched, list_values_entd)
+    
+    return plt
+
+
+def histogram_distance_annuelle_niveau_vie_decile(data_matched, data_entd, decile):
+    data_matched['distance'] = data_matched['distance'].astype(float)
+    data_matched['distance_racine'] = (data_matched['distance']) ** (0.5)
+    distance_racine_max_matched = data_matched['distance_racine'].max()
+
+    data_entd['distance'] = data_entd['distance'].astype(float)
+    data_entd['distance_racine'] = (data_entd['distance']) ** (0.5)
+    distance_racine_max_entd = data_entd['distance_racine'].max()
+
+    distance_racine_max = max(distance_racine_max_matched, distance_racine_max_entd)
+    data_matched['distance_groupe'] = (data_matched['distance_racine'] / distance_racine_max).round(decimals = 1)
+    data_entd['distance_groupe'] = (data_entd['distance_racine'] / distance_racine_max).round(decimals = 1)
+
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+    data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched_decile.query('distance_groupe == {}'.format(j))['pondmen']) /
+            data_matched_decile['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
+    
+    data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd_decile.query('distance_groupe == {}'.format(j))['pondmen']) /
+            data_entd_decile['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    histogrammes(list_keys, list_values_matched, list_values_entd)
+
+    return plt
+
+
+def histogram_distance_diesel_annuelle_niveau_vie_decile(data_matched, data_entd, decile):
+    data_matched['distance_diesel'] = data_matched['distance_diesel'].astype(float)
+    data_matched['distance_diesel_racine'] = (data_matched['distance_diesel']) ** (0.5)
+    distance_diesel_racine_max_matched = data_matched['distance_diesel_racine'].max()
+
+    data_entd['distance_diesel'] = data_entd['distance_diesel'].astype(float)
+    data_entd['distance_diesel_racine'] = (data_entd['distance_diesel']) ** (0.5)
+    distance_diesel_racine_max_entd = data_entd['distance_diesel_racine'].max()
+
+    distance_diesel_racine_max = max(distance_diesel_racine_max_matched, distance_diesel_racine_max_entd)
+    data_matched['distance_diesel_groupe'] = (data_matched['distance_diesel_racine'] / distance_diesel_racine_max).round(decimals = 1)
+    data_entd['distance_diesel_groupe'] = (data_entd['distance_diesel_racine'] / distance_diesel_racine_max).round(decimals = 1)
+
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+    data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched_decile.query('distance_diesel_groupe == {}'.format(j))['pondmen']) /
+            data_matched_decile['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
+    
+    data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd_decile.query('distance_diesel_groupe == {}'.format(j))['pondmen']) /
+            data_entd_decile['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    histogrammes(list_keys, list_values_matched, list_values_entd)
+
+    return plt
+
+
+def histogram_distance_essence_annuelle_niveau_vie_decile(data_matched, data_entd, decile):
+    data_matched['distance_essence'] = data_matched['distance_essence'].astype(float)
+    data_matched['distance_essence_racine'] = (data_matched['distance_essence']) ** (0.5)
+    distance_essence_racine_max_matched = data_matched['distance_essence_racine'].max()
+
+    data_entd['distance_essence'] = data_entd['distance_essence'].astype(float)
+    data_entd['distance_essence_racine'] = (data_entd['distance_essence']) ** (0.5)
+    distance_essence_racine_max_entd = data_entd['distance_essence_racine'].max()
+
+    distance_essence_racine_max = max(distance_essence_racine_max_matched, distance_essence_racine_max_entd)
+    data_matched['distance_essence_groupe'] = (data_matched['distance_essence_racine'] / distance_essence_racine_max).round(decimals = 1)
+    data_entd['distance_essence_groupe'] = (data_entd['distance_essence_racine'] / distance_essence_racine_max).round(decimals = 1)
+
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+    data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched_decile.query('distance_essence_groupe == {}'.format(j))['pondmen']) /
+            data_matched_decile['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
+    
+    data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(decile))
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd_decile.query('distance_essence_groupe == {}'.format(j))['pondmen']) /
+            data_entd_decile['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    histogrammes(list_keys, list_values_matched, list_values_entd)
+
+    return plt
+
+
+
+def histogram_distance_annuelle_tuu(data_matched, data_entd, element):
+    data_matched['distance'] = data_matched['distance'].astype(float)
+    data_matched['distance_racine'] = (data_matched['distance']) ** (0.5)
+    distance_racine_max_matched = data_matched['distance_racine'].max()
+
+    data_entd['distance'] = data_entd['distance'].astype(float)
+    data_entd['distance_racine'] = (data_entd['distance']) ** (0.5)
+    distance_racine_max_entd = data_entd['distance_racine'].max()
+
+    distance_racine_max = max(distance_racine_max_matched, distance_racine_max_entd)
+    data_matched['distance_groupe'] = (data_matched['distance_racine'] / distance_racine_max).round(decimals = 1)
+    data_entd['distance_groupe'] = (data_entd['distance_racine'] / distance_racine_max).round(decimals = 1)
+
+    list_values_matched = []
+    list_values_entd = []
+    list_keys = []
+    data_matched_decile = data_matched.query('tuu == {}'.format(element))
+    for i in range(0,11):
+        j = float(i)/10
+        part_matched = (
+            sum(data_matched_decile.query('distance_groupe == {}'.format(j))['pondmen']) /
+            data_matched_decile['pondmen'].sum()
+            )
+        list_values_matched.append(part_matched)
+    
+    data_entd_decile = data_entd.query('tuu == {}'.format(element))
+    for i in range(0,11):
+        j = float(i)/10
+        part_entd = (
+            sum(data_entd_decile.query('distance_groupe == {}'.format(j))['pondmen']) /
+            data_entd_decile['pondmen'].sum()
+            )
+
+        list_values_entd.append(part_entd)
+        list_keys.append('{}'.format(i))
+
+    print histogrammes(list_keys, list_values_matched, list_values_entd)
+
+    return plt
+
+
 histogram_distance_annuelle(data_matched_distance, data_entd)
+histogram_distance_diesel_annuelle(data_matched_distance, data_entd)
+histogram_distance_essence_annuelle(data_matched_distance, data_entd)
 
-    
-    
-    
+plots_by_decile(histogram_distance_annuelle_niveau_vie_decile, data_matched_random)
+plots_by_tuu(histogram_distance_annuelle_tuu, data_matched_random)
 
-    
-def histogram_distance_annuelle_niveau_vie_decile(data_matched, data_entd):
-    list_values_matched = []
-    list_values_entd = []
-    list_keys = []
-    for i in range(1,11):
-        data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(i))
-        data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(i))
-
-        part_entd = (
-            100 *
-            sum(data_entd_decile['pondmen'] * (data_entd_decile['froid'] == 1)) /
-            sum(data_entd_decile['pondmen'])
-            )
-        part_matched = (
-            100 *
-            sum(data_matched_decile['pondmen'] * (data_matched_decile['froid'] == 1)) /
-            sum(data_matched_decile['pondmen'])
-            )
-
-        list_values_matched.append(part_matched)
-        list_values_entd.append(part_entd)
-        list_keys.append('{}'.format(i))
-
-    histogrammes(list_keys, list_values_matched, list_values_entd)
-    
-    return plt
-
-    
-    
-    
-    
-    
-    
-def histogram_froid_niveau_vie_decile(data_matched, data_entd):
-    list_values_matched = []
-    list_values_entd = []
-    list_keys = []
-    for i in range(1,11):
-        data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(i))
-        data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(i))
-
-        part_entd = (
-            100 *
-            sum(data_entd_decile['pondmen'] * (data_entd_decile['froid'] == 1)) /
-            sum(data_entd_decile['pondmen'])
-            )
-        part_matched = (
-            100 *
-            sum(data_matched_decile['pondmen'] * (data_matched_decile['froid'] == 1)) /
-            sum(data_matched_decile['pondmen'])
-            )
-
-        list_values_matched.append(part_matched)
-        list_values_entd.append(part_entd)
-        list_keys.append('{}'.format(i))
-
-    histogrammes(list_keys, list_values_matched, list_values_entd)
-    
-    return plt
-
-
-def histogram_froid_cout_niveau_vie_decile(data_matched, data_entd):
-    list_values_matched = []
-    list_values_entd = []
-    list_keys = []
-    for i in range(1,11):
-        data_entd_decile = data_entd.query('niveau_vie_decile == {}'.format(i))
-        data_matched_decile = data_matched.query('niveau_vie_decile == {}'.format(i))
-
-        part_entd = (
-            100 *
-            sum(data_entd_decile['pondmen'] * (data_entd_decile['froid_cout'] == 1)) /
-            sum(data_entd_decile['pondmen'])
-            )
-        part_matched = (
-            100 *
-            sum(data_matched_decile['pondmen'] * (data_matched_decile['froid_cout'] == 1)) /
-            sum(data_matched_decile['pondmen'])
-            )
-    
-        list_values_matched.append(part_matched)
-        list_values_entd.append(part_entd)
-        list_keys.append('{}'.format(i))
-
-    histogrammes(list_keys, list_values_matched, list_values_entd)
-    
-    return plt
-
-
-def histogram_froid_tuu(data_matched, data_entd):
-    list_values_matched = []
-    list_values_entd = []
-    list_keys = []
-    for i in range(1,9):
-        data_entd_tuu = data_entd.query('tuu == {}'.format(i))
-        data_matched_tuu = data_matched.query('tuu == {}'.format(i))
-
-        part_entd = (
-            100 *
-            sum(data_entd_tuu['pondmen'] * (data_entd_tuu['froid'] == 1)) /
-            sum(data_entd_tuu['pondmen'])
-            )
-        part_matched = (
-            100 *
-            sum(data_matched_tuu['pondmen'] * (data_matched_tuu['froid'] == 1)) /
-            sum(data_matched_tuu['pondmen'])
-            )
-
-        list_values_matched.append(part_matched)
-        list_values_entd.append(part_entd)
-        list_keys.append('{}'.format(i))
-
-    histogrammes(list_keys, list_values_matched, list_values_entd)
-    
-    return plt
-
-
-histogram_froid_niveau_vie_decile(data_matched_distance, data_entd)
-histogram_froid_niveau_vie_decile(data_matched_random, data_entd)
-
-histogram_froid_cout_niveau_vie_decile(data_matched_distance, data_entd)
-histogram_froid_cout_niveau_vie_decile(data_matched_random, data_entd)
-
-histogram_froid_tuu(data_matched_distance, data_entd)
-histogram_froid_tuu(data_matched_random, data_entd)
-
-
+plots_by_decile(histogram_distance_diesel_annuelle_niveau_vie_decile, data_matched_random)
+plots_by_decile(histogram_distance_essence_annuelle_niveau_vie_decile, data_matched_random)
