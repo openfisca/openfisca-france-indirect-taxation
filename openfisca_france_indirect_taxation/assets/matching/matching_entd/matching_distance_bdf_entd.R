bde66e9a85ba02e7e84f92c07ce278f8f8500dad
Thomas DOUENNE <t.douenne@pse.local>
2017-08-02 16:29:07 +0200

    Improve specification for matching ENTD

--- openfisca_france_indirect_taxation/assets/matching/matching_entd/matching_distance_bdf_entd.R
+++ openfisca_france_indirect_taxation/assets/matching/matching_entd/matching_distance_bdf_entd.R
@@ -1,11 +1,11 @@
 # Import data
-data_entd <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matching_entd.csv", header = -1, sep=",")
-data_bdf <- read.csv(file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matching_bdf.csv", header = -1, sep=",")
+data_entd <- read.csv(file = "C:/Users/t.douenne/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matching_entd.csv", header = -1, sep=",")
+data_bdf <- read.csv(file = "C:/Users/t.douenne/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matching_bdf.csv", header = -1, sep=",")
 
 # Compute random matching
 out.nnd <- NND.hotdeck(
   data.rec = data_bdf, data.don = data_entd,
-  match.vars = c("nb_diesel", "age_vehicule"),
+  match.vars = c("nb_diesel", "agepr", "age_vehicule", "rural", "paris", "npers", "nactifs", "veh_tot"),
   don.class = c("niveau_vie_decile"),
   dist.fun = "Gower"
 )
@@ -14,10 +14,10 @@ out.nnd <- NND.hotdeck(
 fused.nnd.m <- create.fused(
   data.rec = data_bdf, data.don = data_entd,
   mtc.ids = out.nnd$mtc.ids,
-  z.vars = c("distance_autre_carbu", "distance_diesel", "distance_essence")
+  z.vars = c("distance", "distance_autre_carbu", "distance_diesel", "distance_essence")
 )
 
 # Save it as csv
 write.csv(fused.nnd.m,
-          file = "C:/Users/Thomas/Documents/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matched_distance.csv"
+          file = "C:/Users/t.douenne/GitHub/openfisca-france-indirect-taxation/openfisca_france_indirect_taxation/assets/matching/matching_entd/data_matched_distance.csv"
 )
