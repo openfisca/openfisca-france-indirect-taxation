374910bb8c43d12d106803169f9b8bf25d4d3b2b
Thomas DOUENNE <t.douenne@pse.local>
2017-11-07 17:50:29 +0100

    Create new figures from reform

--- openfisca_france_indirect_taxation/examples/master_thesis/energy_expenditures_by_categories.py
+++ openfisca_france_indirect_taxation/examples/master_thesis/energy_expenditures_by_categories.py
@@ -5,26 +5,24 @@ from __future__ import division
 
 
 # Import modules specific to OpenFisca
-from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_line, save_dataframe_to_graph, \
+from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_bar, save_dataframe_to_graph, \
     dataframe_by_group
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.almost_ideal_demand_system.aids_estimation_from_stata import get_elasticities
 from openfisca_france_indirect_taxation.examples.calage_bdf_cn_energy import get_inflators_by_year_energy
 
-#
-inflators_by_year = get_inflators_by_year_energy(rebuild = False)
-year = 2014
+year = 2016
 data_year = 2011
-elasticities = get_elasticities(data_year)
+inflators_by_year = get_inflators_by_year_energy(rebuild = False)
 inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year])
-#del inflation_kwargs['inflator_by_variable']['somme_coicop12']
+elasticities = get_elasticities(data_year)
 
-simulated_variables = ['depenses_carburants', 'depenses_energies_totales', 'depenses_energies_logement'] #['depenses_energies', 'depenses_energies_logement', 'poste_coicop_722']
+simulated_variables = ['depenses_energies_totales', 'depenses_energies_logement', 'depenses_carburants_corrigees'] #['depenses_energies', 'depenses_energies_logement', 'poste_coicop_722']
 
 survey_scenario = SurveyScenario.create(
     elasticities = elasticities,
-    #inflation_kwargs = inflation_kwargs,
-    reform_key = 'taxe_carbone',
+    inflation_kwargs = inflation_kwargs,
+    reform_key = 'officielle_2018_in_2016',
     year = year,
     data_year = data_year
     )
@@ -32,9 +30,9 @@ for category in ['niveau_vie_decile', 'age_group_pr', 'strate']:
     df = dataframe_by_group(survey_scenario, category, simulated_variables, reference = True)
     df.rename(columns = {'depenses_energies_totales': 'Total energy expenditures',
         'depenses_energies_logement': 'Housing energy expenditures',
-        'depenses_carburants': 'Fuel expenditures'},
+        'depenses_carburants_corrigees': 'Fuel expenditures'},
         inplace = True)
 
     # Réalisation de graphiques
-    graph_builder_line(df)
-    #save_dataframe_to_graph(df, 'Expenditures/energy_expenditures_by_{}.csv'.format(category))
+    graph_builder_bar(df, False)
+    save_dataframe_to_graph(df, 'Expenditures/energy_expenditures_by_{}.csv'.format(category))
---
+++ openfisca_france_indirect_taxation/examples/reforme_officielle_2018_in_2016/elasticities_by_categ.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import pandas as pd
+
+from openfisca_france_indirect_taxation.examples.utils_example import \
+    dataframe_by_group, graph_builder_bar, save_dataframe_to_graph, energy_modes
+from openfisca_france_indirect_taxation.surveys import SurveyScenario
+from openfisca_france_indirect_taxation.almost_ideal_demand_system.aids_estimation_from_stata import get_elasticities
+
+from openfisca_france_indirect_taxation.examples.calage_bdf_cn_energy import get_inflators_by_year_energy
+
+
+year = 2016
+data_year = 2011
+inflators_by_year = get_inflators_by_year_energy(rebuild = False)
+inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year])
+elasticities = get_elasticities(data_year)
+
+reforme = 'officielle_2018_in_2016'
+
+survey_scenario = SurveyScenario.create(
+    elasticities = elasticities,
+    inflation_kwargs = inflation_kwargs,
+    reform_key = reforme,
+    year = year,
+    data_year = data_year
+    )
+
+simulated_variables = [
+    'elas_price_1_1',
+    'elas_price_2_2',
+    'elas_price_3_3',
+    'elas_exp_1',
+    'elas_exp_2',
+    'elas_exp_3',
+    'niveau_vie_decile',
+    'strate',
+    'age_group_pr',
+    'pondmen',
+    'combustibles_liquides',
+    'gaz_ville',
+    'depenses_carburants_corrigees',
+    'depenses_energies_logement',
+    'depenses_tot',
+    ]
+
+df = survey_scenario.create_data_frame_by_entity(simulated_variables, period = year)['menage']
+
+df = energy_modes(df)
+
+def elasticities_by_categ(df, categ):
+    i_min = df[categ].min()
+    i_max = df[categ].max()
+    df_to_plot = pd.DataFrame(index = range(i_min, i_max+1), columns = ['elas_transports', 'elas_housing'])
+    for i in range(i_min, i_max+1):
+        df_categ = df.query('{0} == {1}'.format(categ, i))
+        df_to_plot['elas_transports'][i] = df_categ['elas_price_1_1'].mean()
+        df_to_plot['elas_housing'][i] = df_categ['elas_price_2_2'].mean()
+  
+    graph_builder_bar(df_to_plot, False)
+    save_dataframe_to_graph(df_to_plot, 'Elasticities/elasticities_by_{}.csv'.format(categ))
+
+    return df_to_plot
+
+df_to_plot = elasticities_by_categ(df, 'niveau_vie_decile')
+df_to_plot = elasticities_by_categ(df, 'strate')
+df_to_plot = elasticities_by_categ(df, 'age_group_pr')
+df_to_plot = elasticities_by_categ(df, 'energy_mode')
+
+df['part_carbu'] = (df['depenses_carburants_corrigees'] * df['pondmen']) / (df['depenses_carburants_corrigees'] * df['pondmen']).sum()
+
+elas_price_1_ponderee = (df['part_carbu'] * df['elas_price_1_1']).sum()
+elas_exp_1_ponderee = (df['part_carbu'] * df['elas_exp_1']).sum()
+
+
+df['part_housing'] = (df['depenses_energies_logement'] * df['pondmen']) / (df['depenses_energies_logement'] * df['pondmen']).sum()
+elas_price_2_ponderee = (df['part_housing'] * df['elas_price_2_2']).sum()
+elas_exp_2_ponderee = (df['part_housing'] * df['elas_exp_2']).sum()
+
+df['depenses_other'] = df['depenses_tot'] - df['depenses_carburants_corrigees'] - df['depenses_energies_logement']
+df.loc[df['depenses_other'] < 0, 'depenses_other'] = 0
+
+df['part_other'] = (df['depenses_other'] * df['pondmen']) / (df['depenses_other'] * df['pondmen']).sum()
+elas_price_3_ponderee = (df['part_other'] * df['elas_price_3_3']).sum()
+elas_exp_3_ponderee = (df['part_other'] * df['elas_exp_3']).sum()
+
+categ = 'strate'
