f794817a668d092bce1ddf344949886440a1462e
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-05-07 15:38:25 +0200

    Fix numpy dep

--- setup.py
+++ setup.py
@@ -50,12 +50,12 @@ setup(
         ),
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
+        "numpy >= 1.11, <= 1.17",
         'Babel >= 0.9.4',
         'OpenFisca-Core >= 0.5.4',
         'OpenFisca-Survey-Manager >= 0.37.0,<1.0',
         'python-slugify',
         'PyYAML >= 3.10',
-        "numpy >= 1.11", #, <1.16",  # openfisca-survey-manager deps and https://github.com/openfisca/openfisca-survey-manager/pull/79            'pandas >= 0.17',
         "pandas", # <= 0.24.2",
         "statsmodels >= 0.10.1, <1.0",
         ],
