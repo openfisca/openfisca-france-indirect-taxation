6f9fdc461c0269e3685583cd04aaefe2fe65d253
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-05-15 18:07:34 +0200

    Do not use header = -1 in read.csv

--- openfisca_france_indirect_taxation/examples/calage_bdf_cn_bis.py
+++ openfisca_france_indirect_taxation/examples/calage_bdf_cn_bis.py
@@ -136,7 +136,7 @@ def get_inflators_by_year(rebuild = False):
     else:
         re_build_inflators = dict()
         inflators_from_csv = pandas.read_csv(os.path.join(assets_directory, 'inflateurs', 'inflators_by_year_wip.csv'),
-            header = -1)
+            header = None)
         for year in range(2000, 2015):
             inflators_from_csv_by_year = inflators_from_csv[inflators_from_csv[2] == year]
             inflators_to_dict = pandas.DataFrame.to_dict(inflators_from_csv_by_year)
