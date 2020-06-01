b1eafe20300ccb1341eaf7e6a01d3daa43277d7e
claireleroy <claire-leroy@outlook.fr>
2020-06-01 17:05:47 +0200

    Déplace un paramètre vers base.py

---
+++ openfisca_france_indirect_taxation/projects/base.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+elasticite_tabac = -0.5  # valeur provenant de la littérature. Hill & Legoupil (2018) trouvent -0,5 sur la période 1950-2015 et -0,4 sur la période 2000-2015
+
--- openfisca_france_indirect_taxation/projects/budget_2019/simul_reformes_tabac.py
+++ openfisca_france_indirect_taxation/projects/budget_2019/simul_reformes_tabac.py
@@ -7,7 +7,7 @@ from openfisca_france_indirect_taxation.examples.utils_example import (
 from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.examples.calage_bdf_cn_energy import get_inflators_by_year_energy
 from openfisca_france_indirect_taxation.reforms.reforme_tva_2019 import reforme_tva_2019
-
+from openfisca_france_indirect_taxation.projects.base import elasticite_tabac
 
 inflators_by_year = get_inflators_by_year_energy(rebuild = False)
 year = 2016
@@ -44,14 +44,11 @@ df = dataframe_by_group(survey_scenario, category = 'niveau_vie_decile', variabl
 # Calibration du nombre de paquets moyen par ménage
 df['nombre_paquets_moyen'] = paquets_par_menage * (df['depenses_cigarettes'] * 10) / (df['depenses_cigarettes'].sum())
 
-# Elasticité
-elas_tabac = -0.5  # valeur provenant de la littérature. Hill & Legoupil (2018) -0,5 sur la période 1950-2015 et -0,4 sur la période 2000-2015
-
 # REFORME CIGARETTES : Effet d'une augmentation de 80c du prix du paquet en 2018, 50c en avril 2019, puis 50c en novembre, avec élasticité :
 df['depenses_cigarettes_2017'] = df['nombre_paquets_moyen'] * 7.1
-df['depenses_cigarettes_janvier_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elas_tabac) * ((7.9 - 7.1) / 7.1))
-df['depenses_cigarettes_avril_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elas_tabac) * ((8.4 - 7.1) / 7.1))
-df['depenses_cigarettes_novembre_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elas_tabac) * ((8.9 - 7.1) / 7.1))
+df['depenses_cigarettes_janvier_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elasticite_tabac) * ((7.9 - 7.1) / 7.1))
+df['depenses_cigarettes_avril_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elasticite_tabac) * ((8.4 - 7.1) / 7.1))
+df['depenses_cigarettes_novembre_2019'] = df['depenses_cigarettes_2017'] * (1 + (1 + elasticite_tabac) * ((8.9 - 7.1) / 7.1))
 
 # Coût cumulé réformes budget 2018 et 2019 en 2019
 # Pas de notion de contrefactuel ici car la législation ne "vieillit" pas chaque année
@@ -61,9 +58,9 @@ df['cout_reforme_cigarettes_elasticite_2019'] = (
 
 
 # REFORME TABAC A ROULER : Effet d'une augmentation du prix de la bague de tabac de 10.8€ à 11.7€ en avril, puis 12.6€ en novembre, avec élasticité
-df['depenses_tabac_a_rouler_janvier_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elas_tabac) * ((10.8 - 8.8) / 8.8))
-df['depenses_tabac_a_rouler_avril_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elas_tabac) * ((11.7 - 8.8) / 8.8))
-df['depenses_tabac_a_rouler_novembre_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elas_tabac) * ((12.6 - 8.8) / 8.8))
+df['depenses_tabac_a_rouler_janvier_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elasticite_tabac) * ((10.8 - 8.8) / 8.8))
+df['depenses_tabac_a_rouler_avril_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elasticite_tabac) * ((11.7 - 8.8) / 8.8))
+df['depenses_tabac_a_rouler_novembre_2019'] = df['depenses_tabac_a_rouler'] * (1 + (1 + elasticite_tabac) * ((12.6 - 8.8) / 8.8))
 df['cout_reforme_tabac_rouler_elasticite_2019'] = (
     3 * df['depenses_tabac_a_rouler_janvier_2019'] + 7 * df['depenses_tabac_a_rouler_avril_2019'] + 2 * df['depenses_tabac_a_rouler_novembre_2019']
     ) / 12 - df['depenses_tabac_a_rouler']
@@ -78,3 +75,4 @@ graph_builder_bar(df['cout_reforme_rev_disp_loyerimput_elasticite_2019'], False)
 
 # Calculer le budget total
 print("Coût total de la réforme : {} milliards d'euros".format(df['cout_reforme_elasticite_2019'].mean() * df_reforme['pondmen'].sum()/1e9))
+
