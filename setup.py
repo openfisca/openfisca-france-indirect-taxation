f0b706e5c4154d641dcd782d36860de513b69001
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-08-31 17:57:28 +0200

    Fix dev deps

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
 
     extras_require = dict(
         dev = [
-            "autopep8 ==1.4.4",
-            "flake8 >=3.7.0,<3.8.0",
-            # "flake8-print", Bring back when one has time to clear all prints
+           'autopep8 >= 1.4.0, < 1.6.0',
+            'flake8 >= 3.9.0, < 4.0.0',
+            'flake8-bugbear >= 19.3.0, < 20.0.0',
             'matplotlib',
             'nbconvert >= 5.5.0',
             "pytest",  # < 5.0",
@@ -50,13 +50,9 @@ setup(
         ),
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        "numpy >= 1.11, <= 1.17",
-        'Babel >= 0.9.4',
-        'OpenFisca-Core >= 0.5.4',
-        'OpenFisca-Survey-Manager >= 0.37.0,<1.0',
+        'OpenFisca-Core >= 35.4.2',
+        'OpenFisca-Survey-Manager >= 0.45,<1.0',
         'python-slugify',
-        'PyYAML >= 3.10',
-        "pandas",  # <= 0.24.2",
         "statsmodels >= 0.10.1, <1.0",
         'xlrd >= 1.0.0',
         ],
