d266d27cc7866c9bc5282fa9224472ac0616e4b2
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-10-07 14:19:31 +0200

    Ajouye une option skip matching dans le bild_survey_data

--- openfisca_france_indirect_taxation/scripts/build_survey_data.py
+++ openfisca_france_indirect_taxation/scripts/build_survey_data.py
@@ -21,11 +21,11 @@ log = logging.getLogger(app_name)
 
 def main():
     parser = argparse.ArgumentParser()
-    # all_years = [2000, 2005, 2011, 2017]
-    all_years = [2011, 2017]
+    all_years = [2000, 2005, 2011, 2017]
     parser.add_argument('-y', '--years', nargs='+', help = "years of survey to build (default = {})'".format(all_years), default = all_years)
     parser.add_argument('-v', '--verbose', action = 'store_true', default = False, help = "increase output verbosity")
-    parser.add_argument('-p', '--pdb', action = 'store_true', default = False, help = "use ptyhon debugger")
+    parser.add_argument('-p', '--pdb', action = 'store_true', default = False, help = "use python debugger")
+    parser.add_argument('-s', '--skip-matching', action = 'store_true', default = False, help = "skip matching step")
 
     args = parser.parse_args()
     logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
@@ -34,7 +34,7 @@ def main():
     years_calage = [int(year) for year in args.years]
 
     try:
-        run(years_calage)
+        run(years_calage, skip_matching = args.skip_matching)
     except Exception as e:
         if args.pdb:
             pdb.post_mortem(sys.exc_info()[2])
