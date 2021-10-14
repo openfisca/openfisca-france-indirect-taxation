992e0e9e07d5358fa4a6d8c69a3df9ab3c6e16b1
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-10-14 10:45:27 +0200

    Update le test d'agrégats

--- openfisca_france_indirect_taxation/tests/test_masses.py
+++ openfisca_france_indirect_taxation/tests/test_masses.py
@@ -15,19 +15,20 @@ log = logging.getLogger(__name__)
 
 def test_masses(year):
 
-    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2017, 2020))
+    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2011, 2020))
     inflators_by_year[2020] = inflators_by_year[2019]
-    data_year = 2017
+    data_year = 2011
+    
 
     baseline_tax_benefit_system = FranceIndirectTaxationTaxBenefitSystem()
 
     # Recalage des dépenses de cigarettes BDF sur consommations agrégées officielles
-    # reforme_calage = create_reforme_calage_depenses_cigarettes(
-    #     agregat_depenses = nombre_paquets_cigarettes_by_year[int(baseline_year)],
-    #     niveau_calage = 'decile',
-    #     year_calage = baseline_year,
-    #     )
-    # baseline_tax_benefit_system = reforme_calage(baseline_tax_benefit_system)
+    reforme_calage = create_reforme_calage_depenses_cigarettes(
+        agregat_depenses = nombre_paquets_cigarettes_by_year[int(year)],
+        niveau_calage = 'decile',
+        year_calage = year,
+        )
+    baseline_tax_benefit_system = reforme_calage(baseline_tax_benefit_system)
 
     survey_scenario = SurveyScenario.create(
         inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year]),
@@ -52,6 +53,10 @@ def test_masses(year):
         year,
         survey_scenario.compute_aggregate(variable = 'tva_total', period = year) / 1e9
         ))
+    log.info("Masse de droits accises tabacs en {} : {} milliards d'euros".format(
+        year,
+        survey_scenario.compute_aggregate(variable = 'total_tabac_droit_d_accise', period = year) / 1e9
+        ))
         
 if __name__ == '__main__':
     import sys
@@ -60,3 +65,4 @@ if __name__ == '__main__':
     for year in [2017,2018,2019,2020]:
         test_masses(year)
        
+
