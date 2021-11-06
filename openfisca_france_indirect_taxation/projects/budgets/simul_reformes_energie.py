07823079414adaff7cd6700f4a6e963350b26c38
@ChloeLallemand <chloe.lallemand@ipp.eu>
2021-11-06 13:13:11 +0100

    Reset les simul (création de nouvelles simulations pour le cumulé)

--- openfisca_france_indirect_taxation/projects/budgets/simul_reformes_energie.py
+++ openfisca_france_indirect_taxation/projects/budgets/simul_reformes_energie.py
@@ -12,23 +12,16 @@ from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.almost_ideal_demand_system.elasticites_aidsills import get_elasticities_aidsills
 from openfisca_france_indirect_taxation.projects.budgets.reforme_energie_budgets_2018_2019 import officielle_2019_in_2017
 from openfisca_france_indirect_taxation.calibration import get_inflators_by_year_energy
-from matplotlib import pyplot as plt
+
 
 log = logging.getLogger(__name__)
 
-path = "U:/fiscalite_indirecte_budget_22/sans_elasticite"
 
 def simulate_reformes_energie(graph = True):
-    data_year = 2017
-    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2011, 2020),data_year = data_year)
+    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2011, 2020))
     inflators_by_year[2020] = inflators_by_year[2019]
-    inflators_by_year[2021] = inflators_by_year[2019]
-    inflators_by_year[2021] = inflators_by_year[2019]
-    inflators_by_year[2018] = inflators_by_year[2017]
-    inflators_by_year[2018]['depenses_carburants'] = inflators_by_year[2018]['depenses_carburants'] * 1.078
-    inflators_by_year[2018]['depenses_essence'] = inflators_by_year[2018]['depenses_essence'] * 1.078
-    inflators_by_year[2018]['depenses_diesel'] = inflators_by_year[2018]['depenses_diesel'] * 1.078
-    year = 2018
+    year = 2019
+    data_year = 2011
     elasticities = get_elasticities_aidsills(data_year, True)
     inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year])
 
@@ -40,20 +33,13 @@ def simulate_reformes_energie(graph = True):
         'niveau_de_vie',
         'tarifs_sociaux_electricite',
         'tarifs_sociaux_gaz',
-        'pondmen',
-        'depenses_carburants',
-        'depenses_diesel',
-        'depenses_essence',
-        'depenses_carburants_corrigees_officielle_2019_in_2017',
-        'depenses_diesel_corrigees_officielle_2019_in_2017',
-        'depenses_essence_corrigees_officielle_2019_in_2017',
-        'elas_price_1_1'
+        'pondmen'
         ]
 
     baseline_tax_benefit_system = FranceIndirectTaxationTaxBenefitSystem()
     asof(baseline_tax_benefit_system, "2017-12-31")
     survey_scenario = SurveyScenario.create(
-        #elasticities = elasticities,
+        elasticities = elasticities,
         inflation_kwargs = inflation_kwargs,
         baseline_tax_benefit_system = baseline_tax_benefit_system,
         reform = officielle_2019_in_2017,
@@ -68,36 +54,31 @@ def simulate_reformes_energie(graph = True):
 
     # Simulation des effets de différentes réformes
 
-    df['cout_reforme_pures_taxes'] = \
-        df['revenu_reforme_officielle_2019_in_2017']/ df['rev_disponible']
+    df['cout_reforme_pures_taxes'] = (
+        df['revenu_reforme_officielle_2019_in_2017']
+        - df['tarifs_sociaux_electricite'] - df['tarifs_sociaux_gaz']
+        ) / df['rev_disponible']
     df['cout_passage_tarifs_sociaux_cheque_energie_majore_et_etendu'] = (
         df['cheques_energie']
         - df['tarifs_sociaux_electricite'] - df['tarifs_sociaux_gaz']
         ) / df['rev_disponible']
     df['cout_total_reforme'] = (
         - df['cout_reforme_pures_taxes']
-        #+ df['cout_passage_tarifs_sociaux_cheque_energie_majore_et_etendu']
+        + df['cout_passage_tarifs_sociaux_cheque_energie_majore_et_etendu']
         )
 
     if graph:
         graph_builder_bar(df['cout_total_reforme'], False)
-        plt.bar(df.index,df['cout_total_reforme'])
-        plt.savefig('{}/variation_reforme_energie_2017_2018'.format(path))
-        plt.close()
-        plt.bar(df.index,df['revenu_reforme_officielle_2019_in_2017'])
-        plt.savefig('{}/revenu_reforme_enerige_2019_in_2017'.format(path))
-        plt.close()
 
     log.info("Coût total de la réforme : {} milliards d'euros".format(
-        (survey_scenario.compute_aggregate('revenu_reforme_officielle_2019_in_2017',period = 2019))
-
-        ))
+        df['cout_total_reforme'].mean() * df_reforme['pondmen'].sum() / 1e9)
+        )
     return df
 
 
 if __name__ == "__main__":
     import sys
     logging.basicConfig(level = logging.INFO, stream = sys.stdout)
-    #from openfisca_france_indirect_taxation.tests.budgets.budget_2019 import test_plf_2019_reformes_energie
-    #test_plf_2019_reformes_energie()
+    from openfisca_france_indirect_taxation.tests.budgets.budget_2019 import test_plf_2019_reformes_energie
+    test_plf_2019_reformes_energie()
     df = simulate_reformes_energie()
--- openfisca_france_indirect_taxation/projects/budgets/simul_reformes_tabac.py
+++ openfisca_france_indirect_taxation/projects/budgets/simul_reformes_tabac.py
@@ -6,42 +6,27 @@ from openfisca_france_indirect_taxation.surveys import SurveyScenario
 from openfisca_france_indirect_taxation.calibration import get_inflators_by_year_energy
 from openfisca_france_indirect_taxation.examples.utils_example import graph_builder_bar, dataframe_by_group
 from openfisca_france_indirect_taxation.projects.base import nombre_paquets_cigarettes_by_year
-from openfisca_france_indirect_taxation.projects.calage_depenses_cigarettes import create_reforme_calage_depenses_cigarettes,create_reforme_calage_depenses_tabac
+from openfisca_france_indirect_taxation.projects.calage_depenses_cigarettes import create_reforme_calage_depenses_cigarettes
 from openfisca_france_indirect_taxation.reforms.reforme_tabac import create_reforme_tabac
-from matplotlib import pyplot as plt
 
 
 log = logging.getLogger(__name__)
 
-path = "U:/fiscalite_indirecte_budget_22/avec_elasticite"
-
-agregat_depenses = {
-    2017 : 20441000000,
-    2018 : 21722000000,
-    2019 : 22531000000,
-    2020 : 22531000000,
-    }
 
 def simulate_reforme_tabac(year, baseline_year, graph = True):
-    data_year = 2017
-    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2011, 2020), data_year = data_year)
+
+    inflators_by_year = get_inflators_by_year_energy(rebuild = True, year_range = range(2011, 2020))
     inflators_by_year[2020] = inflators_by_year[2019]
-    inflators_by_year[2021] = inflators_by_year[2020]
-    inflators_by_year[2022] = inflators_by_year[2020]
+    data_year = 2011
 
     baseline_tax_benefit_system = FranceIndirectTaxationTaxBenefitSystem()
 
     # Recalage des dépenses de cigarettes BDF sur consommations agrégées officielles
     reforme_calage = create_reforme_calage_depenses_cigarettes(
-        agregat_depenses = nombre_paquets_cigarettes_by_year[int(baseline_year)], #nombre_paquets_cigarettes_by_year[int(baseline_year)],
+        agregat_depenses = nombre_paquets_cigarettes_by_year[int(baseline_year)],
         niveau_calage = 'decile',
         year_calage = baseline_year,
         )
-    reforme_calage = create_reforme_calage_depenses_tabac(
-        agregat_depenses = agregat_depenses[int(baseline_year)], #nombre_paquets_cigarettes_by_year[int(baseline_year)],
-        year_calage = baseline_year,
-        )
-
     baseline_tax_benefit_system = reforme_calage(baseline_tax_benefit_system)
 
     # Applicatin des réformes de la fiscalité tabac
@@ -72,30 +57,20 @@ def simulate_reforme_tabac(year, baseline_year, graph = True):
         / df['rev_disp_loyerimput']
         )
     if graph:
-        #graph_builder_bar(diff['variation_relative_depenses_tabac'], False)
-        plt.bar(diff.index,diff['variation_relative_depenses_tabac'])
-        plt.savefig('{}/variation_relative_depenses_tabac_{}_{}_elas_0.78.png'.format(path,baseline_year,year))
-        plt.close()
-        #graph_builder_bar(diff['depenses_tabac'], False)
-        plt.bar(diff.index,diff['depenses_tabac'])
-        plt.close()
-    cout = survey_scenario.compute_aggregate(variable = 'depenses_tabac', period = year, difference = True) / 1e9
+        graph_builder_bar(diff['variation_relative_depenses_tabac'], False)
     log.info("Coût total de la réforme (baseline : {}): {} milliards d'euros".format(
-        baseline_year, cout
-        
+        baseline_year,
+        survey_scenario.compute_aggregate(variable = 'depenses_tabac', period = year, difference = True) / 1e9
         ))
 
-    return diff['variation_relative_depenses_tabac'].values, cout
+    return diff['variation_relative_depenses_tabac'].values
 
 
 if __name__ == '__main__':
     import sys
     logging.basicConfig(level = logging.INFO, stream = sys.stdout)
-    
-    couts = {}
-    for baseline_year in ['2018']:#['2017','2018','2019','2020']:
-        year = 2019#int(baseline_year) + 1
-        #test_plf_2019_reforme_tabac(baseline_year)
-        variation_relative_depenses_tabac, cout = simulate_reforme_tabac(year = year, baseline_year = baseline_year)
-        couts["{}_{}".format(baseline_year, str(year))] = cout
-        
+
+    for baseline_year in ['2017', '2018']:
+        from openfisca_france_indirect_taxation.tests.budgets.budget_2019 import test_plf_2019_reforme_tabac
+        test_plf_2019_reforme_tabac(baseline_year)
+        variation_relative_depenses_tabac = simulate_reforme_tabac(year = 2019, baseline_year = baseline_year)
