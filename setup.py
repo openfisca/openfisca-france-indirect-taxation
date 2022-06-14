8b707510d94bfa2c597dab82319d40048169c9d9
Emmanuel Raviart <emmanuel@raviart.com>
2022-06-14 16:59:38 +0200

    Upgrade packages.

--- setup.py
+++ setup.py
@@ -36,12 +36,12 @@ setup(
     extras_require = dict(
         dev = [
             'autopep8 >= 1.4.0, < 1.6.0',
-            'flake8 >= 3.9.0, < 4.0.0',
+            'flake8 >= 4.0.0, < 5.0.0',
             'flake8-bugbear >= 19.3.0, < 20.0.0',
             'ipdb >=0.13.9, < 1.0.0',
             'matplotlib <= 3.4.3, < 4.0.0',
             'nbconvert >= 5.5.0, < 6.0.0',
-            "pytest < 6.0",
+            'pytest >= 5.0.0, < 7.0.0',
             'ruamel.yaml >= 0.17.16, < 1.0.0',
             'seaborn >= 0.11.2, < 1.0.0',
             ],
