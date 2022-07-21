bc7e07364e9568fd56c978270149970c5c5737bd
kendrick <herzbergkendrick@gmail.com>
2022-07-21 15:01:45 +0200

    renforcement variables

--- openfisca_france_indirect_taxation/variables/taxes_indirectes/ticpe_carburants.py
+++ openfisca_france_indirect_taxation/variables/taxes_indirectes/ticpe_carburants.py
@@ -15,7 +15,7 @@ class gazole_b7_ticpe(Variable):
     label = 'Calcul du montant de TICPE sur le gazole B7'
     definition_period = YEAR
 
-    def formula(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         code_region = menage('code_region', period)
         accise_gazole_b7 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.gazole
         majoration_ticpe_gazole_b7 = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_gazole
@@ -33,6 +33,27 @@ class gazole_b7_ticpe(Variable):
             dtype=np.float32)
         major_mobilites_tipce_gazole = parameters(period).imposition_indirecte.produits_energetiques.majoration_ile_de_france_mobilites_ticpe.major_mobilites_tipce_gazole
         accise_gazole_b7_total = accise_gazole_b7 + majoration_regionale_ticpe_gazole_b7 - (maximum_value_affectation - affectation_regionale_ticpe_gazole_b7) + ((code_region == '11') * major_mobilites_tipce_gazole)
+        nombre_litres_gazole_b7 = menage('nombre_litres_gazole_b7', period)
+        montant_gazole_b7_ticpe = nombre_litres_gazole_b7 * (accise_gazole_b7_total / 100)
+        return montant_gazole_b7_ticpe
+
+    def formula(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_gazole_b7 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.gazole
+        majoration_ticpe_gazole_b7 = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_gazole
+        majoration_regionale_ticpe_gazole_b7 = np.fromiter(
+            (
+                getattr(majoration_ticpe_gazole_b7, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_gazole.maximum_value_affectation
+        affectation_ticpe_gazole_b7 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_gazole
+        affectation_regionale_ticpe_gazole_b7 = np.fromiter(
+            (
+                getattr(affectation_ticpe_gazole_b7, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        accise_gazole_b7_total = accise_gazole_b7 + majoration_regionale_ticpe_gazole_b7 - (maximum_value_affectation - affectation_regionale_ticpe_gazole_b7)
         nombre_litres_gazole_b7 = menage('nombre_litres_gazole_b7', period)
         montant_gazole_b7_ticpe = nombre_litres_gazole_b7 * (accise_gazole_b7_total / 100)
         return montant_gazole_b7_ticpe
@@ -81,7 +102,7 @@ class essence_sp95_e10_ticpe(Variable):  # paru au Journal Officiel le 31 janvie
     label = "Calcul du montant de la TICPE sur l'essence SP95 E10"
     definition_period = YEAR
 
-    def formula_2009(menage, period, parameters):
+    def formula_2019(menage, period, parameters):
         code_region = menage('code_region', period)
         accise_super_e10 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_e10
         major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
@@ -104,6 +125,49 @@ class essence_sp95_e10_ticpe(Variable):  # paru au Journal Officiel le 31 janvie
         montant_sp_e10_ticpe = nombre_litres_essence_sp95_e10 * (accise_sp_e10_ticpe / 100)
         return montant_sp_e10_ticpe
 
+    def formula_2017(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super_e10 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_e10
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_e10.maximum_value_affectation
+        affectation_ticpe_sp_95_e10 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_e10
+        affectation_regionale_ticpe_sp95_e10 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_e10, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        major_mobilites_tipce_sp95_e10 = parameters(period).imposition_indirecte.produits_energetiques.majoration_ile_de_france_mobilites_ticpe.major_mobilites_tipce_sp95_e10
+        accise_sp_e10_ticpe = accise_super_e10 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_e10) + ((code_region == '11') * major_mobilites_tipce_sp95_e10)
+        nombre_litres_essence_sp95_e10 = menage('nombre_litres_essence_sp95_e10', period)
+        montant_sp_e10_ticpe = nombre_litres_essence_sp95_e10 * (accise_sp_e10_ticpe / 100)
+        return montant_sp_e10_ticpe
+
+    def formula_2009(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super_e10 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_e10
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_e10.maximum_value_affectation
+        affectation_ticpe_sp_95_e10 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_e10
+        affectation_regionale_ticpe_sp95_e10 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_e10, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        accise_sp_e10_ticpe = accise_super_e10 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_e10)
+        nombre_litres_essence_sp95_e10 = menage('nombre_litres_essence_sp95_e10', period)
+        montant_sp_e10_ticpe = nombre_litres_essence_sp95_e10 * (accise_sp_e10_ticpe / 100)
+        return montant_sp_e10_ticpe
+
 
 class essence_sp95_ticpe(Variable):
     value_type = float
@@ -111,7 +175,7 @@ class essence_sp95_ticpe(Variable):
     label = "Calcul du montant de TICPE sur l'essence SP95"
     definition_period = YEAR
 
-    def formula(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         code_region = menage('code_region', period)
         accise_super95 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
         major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
@@ -134,6 +198,49 @@ class essence_sp95_ticpe(Variable):
         montant_sp95_ticpe = nombre_litres_essence_sp95 * (accise_sp_95_ticpe / 100)
         return montant_sp95_ticpe
 
+    def formula_2002(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super95 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98.maximum_value_affectation
+        affectation_ticpe_sp_95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98
+        affectation_regionale_ticpe_sp95_sp98 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_sp98, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        refraction_corse_tipce_sp95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.refraction_corse_ticpe.refraction_corse_tipce_sp95_sp98
+        accise_sp_95_ticpe = accise_super95 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_sp98) - ((code_region == '94') * refraction_corse_tipce_sp95_sp98)
+        nombre_litres_essence_sp95 = menage('nombre_litres_essence_sp95', period)
+        montant_sp95_ticpe = nombre_litres_essence_sp95 * (accise_sp_95_ticpe / 100)
+        return montant_sp95_ticpe
+
+    def formula(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super95 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98.maximum_value_affectation
+        affectation_ticpe_sp_95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98
+        affectation_regionale_ticpe_sp95_sp98 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_sp98, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        accise_sp_95_ticpe = accise_super95 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_sp98)
+        nombre_litres_essence_sp95 = menage('nombre_litres_essence_sp95', period)
+        montant_sp95_ticpe = nombre_litres_essence_sp95 * (accise_sp_95_ticpe / 100)
+        return montant_sp95_ticpe
+
 
 class essence_sp98_ticpe(Variable):
     value_type = float
@@ -141,7 +248,7 @@ class essence_sp98_ticpe(Variable):
     label = "Calcul du montant de TICPE sur l'essence SP98"
     definition_period = YEAR
 
-    def formula(menage, period, parameters):
+    def formula_2017(menage, period, parameters):
         code_region = menage('code_region', period)
         accise_super98 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
         major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
@@ -164,6 +271,49 @@ class essence_sp98_ticpe(Variable):
         montant_sp98_ticpe = nombre_litres_essence_sp98 * (accise_sp_98_ticpe / 100)
         return montant_sp98_ticpe
 
+    def formula_2002(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super95 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98.maximum_value_affectation
+        affectation_ticpe_sp_95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98
+        affectation_regionale_ticpe_sp95_sp98 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_sp98, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        refraction_corse_tipce_sp95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.refraction_corse_ticpe.refraction_corse_tipce_sp95_sp98
+        accise_sp_95_ticpe = accise_super95 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_sp98) - ((code_region == '94') * refraction_corse_tipce_sp95_sp98)
+        nombre_litres_essence_sp95 = menage('nombre_litres_essence_sp95', period)
+        montant_sp95_ticpe = nombre_litres_essence_sp95 * (accise_sp_95_ticpe / 100)
+        return montant_sp95_ticpe
+
+    def formula(menage, period, parameters):
+        code_region = menage('code_region', period)
+        accise_super95 = parameters(period).imposition_indirecte.produits_energetiques.ticpe.super_95_98
+        major_regionale_ticpe_super = parameters(period).imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10
+        majoration_ticpe_super = np.fromiter(
+            (
+                getattr(major_regionale_ticpe_super, region_cell, 0)
+                for region_cell in code_region),
+            dtype=np.float32)
+        maximum_value_affectation = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98.maximum_value_affectation
+        affectation_ticpe_sp_95_sp98 = parameters(period).imposition_indirecte.produits_energetiques.affectation_regionale_ticpe_sp95_sp98
+        affectation_regionale_ticpe_sp95_sp98 = np.fromiter(
+            (
+                getattr(affectation_ticpe_sp_95_sp98, region_cell, maximum_value_affectation)
+                for region_cell in code_region),
+            dtype=np.float32)
+        accise_sp_95_ticpe = accise_super95 + majoration_ticpe_super - (maximum_value_affectation - affectation_regionale_ticpe_sp95_sp98)
+        nombre_litres_essence_sp95 = menage('nombre_litres_essence_sp95', period)
+        montant_sp95_ticpe = nombre_litres_essence_sp95 * (accise_sp_95_ticpe / 100)
+        return montant_sp95_ticpe
+
 
 class essence_super_plombe_ticpe(Variable):
     value_type = float
@@ -172,12 +322,18 @@ class essence_super_plombe_ticpe(Variable):
     definition_period = YEAR
     end = '2006-12-31'
 
-    def formula(menage, period, parameters):
+    def formula_2002(menage, period, parameters):
         code_region = menage('code_region', period)
         accise_super_plombe_ticpe = parameters(period.start).imposition_indirecte.produits_energetiques.ticpe.super_plombe
         refraction_corse_tipce_super_plombe = parameters(period).imposition_indirecte.produits_energetiques.refraction_corse_ticpe.refraction_corse_tipce_super_plombe
         nombre_litres_essence_super_plombe = menage('nombre_litres_essence_super_plombe', period)
         montant_super_plombe_ticpe = nombre_litres_essence_super_plombe * (accise_super_plombe_ticpe / 100) - ((code_region == '94') * refraction_corse_tipce_super_plombe)
+        return montant_super_plombe_ticpe
+
+    def formula(menage, period, parameters):
+        accise_super_plombe_ticpe = parameters(period.start).imposition_indirecte.produits_energetiques.ticpe.super_plombe
+        nombre_litres_essence_super_plombe = menage('nombre_litres_essence_super_plombe', period)
+        montant_super_plombe_ticpe = nombre_litres_essence_super_plombe * (accise_super_plombe_ticpe / 100)
         return montant_super_plombe_ticpe
 
 
