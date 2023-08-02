e380a60d81b6a0fdd9d5b5d02442081ac243a3d7
Emmanuel Raviart <emmanuel@raviart.com>
2023-08-02 11:10:18 +0200

    Fix location

--- openfisca_france_indirect_taxation/location.py
+++ openfisca_france_indirect_taxation/location.py
@@ -1,6 +1,3 @@
-from importlib import metadata
 from pathlib import Path
 
-openfisca_france_indirect_taxation_location = Path(
-    metadata.distribution('openfisca-france-indirect-taxation').files[0]
-    ).parent
+openfisca_france_indirect_taxation_location = Path(__file__).parent.parent
