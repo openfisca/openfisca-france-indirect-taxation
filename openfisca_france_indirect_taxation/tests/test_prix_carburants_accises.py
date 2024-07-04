8a40d309649d4b70f04397536c8be16982036088
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2024-07-04 11:31:27 +0200

    wip

--- openfisca_france_indirect_taxation/tests/test_prix_carburants_accises.py
+++ openfisca_france_indirect_taxation/tests/test_prix_carburants_accises.py
@@ -11,14 +11,14 @@ def test_prix_carburants_2022():
     date = "2022-11-01"
     parameters = FranceIndirectTaxationTaxBenefitSystem().parameters(date)
     assert round(parameters.prix_carburants.super_95_ttc, 2) == 183.0
-    assert round(parameters.prix_carburants.super_95_e10_ttc, 2) == 183.0
+    assert round(parameters.prix_carburants.super_95_e10_ttc, 2) == 178.0
     accise_super95 = parameters.imposition_indirecte.produits_energetiques.ticpe.super_95_98
     majoration_ticpe_super95 = parameters.imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10.alsace
-    assert round(accise_super95 + majoration_ticpe_super95, 2) == 63.19
+    assert round(accise_super95 + majoration_ticpe_super95, 2) == 587.0
     accise_super_e10 = parameters.imposition_indirecte.produits_energetiques.ticpe.super_e10
     majoration_ticpe_super_e10 = \
         parameters.imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10.alsace
-    assert round(accise_super_e10 + majoration_ticpe_super_e10, 2) == 63.19
+    assert round(accise_super_e10 + majoration_ticpe_super_e10, 2) == 123.19
 
 def test_prix_carburants_2024():
     '''
@@ -28,11 +28,11 @@ def test_prix_carburants_2024():
     year = "2024"
     parameters = FranceIndirectTaxationTaxBenefitSystem().parameters(year)
     assert round(parameters.prix_carburants.super_95_ttc, 2) == 183.0
-    assert round(parameters.prix_carburants.super_95_e10_ttc, 2) == 183.0
+    assert round(parameters.prix_carburants.super_95_e10_ttc, 2) == 178.0
     accise_super95 = parameters.imposition_indirecte.produits_energetiques.ticpe.super_95_98
     majoration_ticpe_super95 = parameters.imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10.alsace
-    assert round(accise_super95 + majoration_ticpe_super95, 2) == 63.19
+    assert round(accise_super95 + majoration_ticpe_super95, 2) == 1234.19
     accise_super_e10 = parameters.imposition_indirecte.produits_energetiques.ticpe.super_e10
     majoration_ticpe_super_e10 = \
         parameters.imposition_indirecte.produits_energetiques.majoration_regionale_ticpe_sp95_sp98_sp95_e10.alsace
-    assert round(accise_super_e10 + majoration_ticpe_super_e10, 2) == 63.19
+    assert round(accise_super_e10 + majoration_ticpe_super_e10, 2) == 12345.19
