508f0f34cd897e546a29e905b62e9c27aaf81b5f
Hervédarr31 <herve.darricau@ensae.fr>
2025-12-30 17:58:25 +0100

    Create functions to build the bdf nomenclature, adjust it to CN and associate a legislation

---
+++ openfisca_france_indirect_taxation/scripts/build_bdf_legislation.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import os
+
+from openfisca_france_indirect_taxation.utils import assets_directory
+
+from openfisca_france_indirect_taxation.scripts.new_build_coicop_legislation import new_apply_modification
+from openfisca_france_indirect_taxation.scripts import build_bdf_nomenclature
+
+
+legislation_directory = os.path.join(assets_directory, 'legislation')
+bdf_nomenclature = pd.read_csv(
+    os.path.join(legislation_directory, 'bdf_2017_nomenclature.csv')
+    )
+
+
+def add_fiscal_categories_to_bdf_nomenclature(bdf_nomenclature, to_csv=False):
+    """
+    Ajoute des catégories fiscales au DataFrame bdf_nomenclature en fonction de règles spécifiques
+    pour chaque code coicop. Les règles incluent des périodes de validité (start, stop) et des origines.
+
+    Args:
+        bdf_nomenclature (pd.DataFrame): DataFrame contenant les codes coicop.
+        to_csv (bool): Si True, sauvegarde le résultat dans un fichier CSV.
+
+    Returns:
+        pd.DataFrame: DataFrame mis à jour avec les catégories fiscales.
+    """
+    # Ajoute les colonnes nécessaires si elles n'existent pas
+    for col in ['start', 'stop', 'origin']:
+        if col not in bdf_nomenclature.columns:
+            bdf_nomenclature[col] = 0
+    bdf_nomenclature['origin'] = 'BDF'
+
+    # Définition des règles fiscales pour chaque code BDF
+    fiscal_rules = [
+        # 01 Produits alimentaires et boissons non alcoolisées
+        {'value': '01', 'categorie_fiscale': 'tva_taux_reduit'},
+        {'value': '01.1.5.2', 'categorie_fiscale': 'tva_taux_plein'},                         # Margarine
+        {'value': '01.1.5.5', 'categorie_fiscale': 'tva_taux_reduit', 'origin': 'TAXIPP'},    # Saindoux autres graisses d'origine animale    
+        {'value': '01.1.8.4', 'categorie_fiscale': 'tva_taux_plein'},                         # Confiserie (hors cocolats)
+
+        # 02 Boissons alcoolisées et tabac
+        {'value': '02.1.1', 'categorie_fiscale': 'alcools_forts'},
+        {'value': '02.1.2', 'categorie_fiscale': 'vin'},                                        # Vins
+        {'value': '02.1.3', 'categorie_fiscale': 'biere'},                                      # Bières
+        {'value': '02.2.1.1', 'categorie_fiscale': 'cigarettes', 'origin': 'TAXIPP'},           # Cigarettes
+        {'value': '02.2.1.2', 'categorie_fiscale': 'cigares', 'origin': 'TAXIPP'},              # Cigares et cigarillos
+        {'value': '02.2.1.3', 'categorie_fiscale': 'tabac_a_rouler', 'origin': 'TAXIPP'},       # Tabac à rouler
+        {'value': '02.3', 'categorie_fiscale': '', 'origin': 'COICOP UN'},                      # Stupéfiants
+        {'value': '02.4', 'categorie_fiscale': '', 'origin': 'TAXIPP'},                         # Alcools, tabacs et stupéfiants offerts à un autre
+
+        # 03 Habillement et chaussures
+        {'value': '03', 'categorie_fiscale': 'tva_taux_plein'},
+
+        # 04 Logement, eau, gaz, électricité et autres combustibles
+        {'value': '04', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '04.5.4.1', 'categorie_fiscale': 'tva_taux_reduit', 'start': 1998, 'stop': 2011},                     # Combustible solide de résidence principale
+        {'value': '04.5.4.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': ['04.4.1.1', '04.4.2.1', '04.4.3.1'], 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},          # Enlèvement des ordures ménagères, assainissement et distribution d'eau,
+        # qui sont au taux réduit de 1994 à 2011
+        {'value': ['04.4.1.1', '04.4.2.1'], 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},              # avant de passer au taux intermédiaire
+        {'value': '04.4.3.1', 'categorie_fiscale': 'tva_taux_reduit', 'start': 2012},                                   # sauf l'eau qui reste à taux réduit                             
+        {'value': ['04.1.1.1', '04.1.2.1'], 'categorie_fiscale': ''},                                                   # Loyers et charges locatives
+        {'value': '04.3.2.1', 'categorie_fiscale': 'tva_taux_intermediaire'},                                           # Services d'entretien et réparation de logements
+
+        # 05 Ameublement, équipement ménager et entretien courant de la maison
+        {'value': '05', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '05.6.2.1', 'categorie_fiscale': 'tva_taux_intermediaire'},                                           # Services domestiques (ménage, garde enfant, jardinage)
+
+        # 06 Santé
+        {'value': '06', 'categorie_fiscale': ''},
+        {'value': '06.1.1.1', 'categorie_fiscale': 'tva_taux_super_reduit'},                                            # Pharmacie
+        {'value': '06.1.1.2', 'categorie_fiscale': 'tva_taux_plein'},                                                   # Parapharmacie                
+        {'value': '06.1.1.3', 'categorie_fiscale': 'tva_taux_reduit'},                                                  # Matériel thérapeutique
+
+        # 07 Transports
+        {'value': '07', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '07.2.2', 'categorie_fiscale': 'ticpe'},                                                              # Carburants et lubrifiants pour véhicules de tourisme
+        {'value': '07.3.1', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                      # Transport ferroviaire de passagers  
+        {'value': '07.3.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '07.3.2', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                      # Transport routier de passagers
+        {'value': '07.3.2', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '07.3.3', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2024},                                      # Transport aérien de passagers
+        {'value': '07.3.3', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '07.3.4', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                      # Transport maritime et fluvial de passagers
+        {'value': '07.3.4', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '07.3.5', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                      # Transport combine de passagers
+        {'value': '07.3.5', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '07.3.6', 'categorie_fiscale': 'tva_taux_plein'},                                                 # Autres services de transports (yc déménagements)
+        # 08 Communications
+        {'value': '08', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '08.1.1.1', 'categorie_fiscale': ''},                                                                # Services postaux
+
+        # 09 Loisirs et culture
+        {'value': '09', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '09.3.1.2', 'categorie_fiscale': 'tva_taux_plein', 'start': 1994},                                   # equipement sportif
+        {'value': '09.3.2.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2016},                           # Horticulture, floriculture
+        {'value': '09.4.1', 'categorie_fiscale': 'tva_taux_reduit', 'start': 1994},                                    # Services récréatifs et sportifs
+        {'value': '09.4.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2015},
+        {'value': '09.4.2.1', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                   # Cinemas, théâtres, concerts
+        {'value': '09.4.2.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012, 'stop': 2013},
+        {'value': '09.4.2.1', 'categorie_fiscale': 'tva_taux_reduit', 'start': 2014},
+        {'value': '09.4.2.2', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                   # Musées, zoos
+        {'value': '09.4.2.2', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '09.4.2.2', 'categorie_fiscale': 'tva_taux_reduit', 'start': 2018},
+        {'value': '09.4.2.3', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2011},                                   # Services de télévision et radiodiffusion
+        {'value': '09.4.2.3', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '09.4.2.4', 'categorie_fiscale': 'tva_taux_plein', 'start': 1994},                                   # Autres services culturels.
+        {'value': '09.4.3', 'categorie_fiscale': '', 'origin': 'COICOP UN'},                                           # Jeux de hasard
+        {'value': '09.5.1', 'categorie_fiscale': 'tva_taux_reduit', 'stop': 2024},                                     # Livre
+        {'value': '09.5.2', 'categorie_fiscale': 'tva_taux_super_reduit'},                                             # Journaux et publications périodiques
+        {'value': '09.6.1.1', 'categorie_fiscale': '', 'start': 2019},                                                 # Voyage à forfait
+
+        # 10 Éducation
+        {'value': '10', 'categorie_fiscale': ''},
+
+        # 11 Hôtellerie restauration
+        {'value': '11', 'categorie_fiscale': 'tva_taux_reduit'},
+        {'value': '11.1.1.1', 'categorie_fiscale': 'tva_taux_plein', 'stop': 2009},                                   # Restauration  (hors boissons alcoolisées)
+        {'value': '11.1.1.1', 'categorie_fiscale': 'tva_taux_reduit', 'start': 2010, 'stop': 2011},
+        {'value': '11.1.1.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '11.1.1.2', 'categorie_fiscale': 'tva_taux_plein'},                                                 # Boissons alcoolisées (cafés, bars et assimilés)
+        {'value': '11.1.2', 'categorie_fiscale': 'tva_taux_reduit'},                                                  # Restauration collective
+        {'value': '11.2.1', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},                            # Hébergement touristique   
+
+        # 12 Autres biens et services
+        {'value': '12', 'categorie_fiscale': 'tva_taux_plein'},
+        {'value': '12.1.3.1', 'categorie_fiscale': '', 'origin': 'COICOP UN'},                                              # Prostitution
+        {'value': '12.4', 'categorie_fiscale': 'tva_taux_reduit', 'start': 2000, 'stop': 2024},                         # Protection sociale
+        {'value': '12.4', 'categorie_fiscale': 'tva_taux_intermediaire', 'start': 2012},
+        {'value': '12.5.1', 'categorie_fiscale': 'autres_assurances', 'origin': 'COICOP UN'},                           # Assurance vie
+        {'value': '12.5.2', 'categorie_fiscale': 'autres_assurances'},                                                  # Autres assurances
+        {'value': '12.5.3', 'categorie_fiscale': 'assurance_sante'},                                                    # Assurance santé
+        {'value': '12.5.4', 'categorie_fiscale': 'assurance_transport'},                                                # Assurance transport
+        {'value': '12.5.5', 'categorie_fiscale': 'autres_assurances', 'origin': 'COICOP UN'},                           # Autres assurances
+        {'value': '12.6', 'categorie_fiscale': ''},                                                                     # Services financiers  
+        
+        # 13 Impôts et taxes, gros travaux, remboursement pret, cadeaux, prelevement employeur, epargne
+        {'value': '13', 'categorie_fiscale': ''},
+        # 14 Allocations logement recues par le menage
+        {'value': '14', 'categorie_fiscale': ''}]
+
+    # Applique chaque règle fiscale
+    for rule in fiscal_rules:
+        value = rule.get('value')
+        if isinstance(value, list):
+            # Si 'value' est une liste, applique la règle à chaque code individuellement
+            for single_value in value:
+                single_rule = rule.copy()
+                single_rule['value'] = single_value
+                bdf_nomenclature = new_apply_modification(bdf_nomenclature, **single_rule)
+        else:
+            # Sinon, applique la règle normalement
+            bdf_nomenclature = new_apply_modification(bdf_nomenclature, **rule)
+
+    # Sauvegarde le résultat dans un fichier CSV si demandé
+    if to_csv:
+        output_path = os.path.join(legislation_directory, 'bdf_2017_legislation.csv')
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)        # Crée le répertoire s'il n'existe pas
+        bdf_nomenclature.to_csv(output_path, index=False)
+
+    return bdf_nomenclature.copy()
+
+
+def test_bdf_legislation():
+    bdf_nomenclature = build_bdf_nomenclature.build_complete_bdf_nomenclature()
+    bdf_legislation = add_fiscal_categories_to_bdf_nomenclature(bdf_nomenclature, to_csv = True)
+    if bdf_legislation.categorie_fiscale.isnull().any():
+        return bdf_legislation.loc[bdf_legislation.categorie_fiscale.isnull()]
+
+
+if __name__ == '__main__':
+    bdf_nomenclature = build_bdf_nomenclature.build_complete_bdf_nomenclature()
+    bdf_legislation = add_fiscal_categories_to_bdf_nomenclature(bdf_nomenclature, to_csv = True)
+    test_bdf_legislation()
---
+++ openfisca_france_indirect_taxation/scripts/build_bdf_nomenclature.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import os
+
+from openfisca_france_indirect_taxation.utils import assets_directory
+
+
+def build_raw_bdf_nomenclature(year = 2017):
+    """Build the raw BDF nomenclature DataFrame from the Excel file of BDF 2017 nomenclature.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the complete BDF nomenclature with
+                      columns for division, group, class, and subclass labels
+                      and codes.
+    """
+    assert year == 2017  # Currently only implemented for 2017
+
+    file_path = os.path.join(
+        assets_directory,
+        'legislation',
+        'Table_bdf.xls',
+        )
+
+    table_bdf = pd.read_excel(file_path, skiprows=3)
+
+    # Get divisions 
+    table_bdf['Division'] = table_bdf['Code sur\n2, 3, 4 positions'].str.endswith('***')
+    bdf_division = table_bdf.loc[table_bdf['Division'] == True, ['Code sur\n2, 3, 4 positions', 'Rubriques']]
+    bdf_division.rename(columns={'Code sur\n2, 3, 4 positions': 'Code_division', 'Rubriques': 'Label_division'}, inplace=True)
+    bdf_division['Label_division'] = bdf_division['Label_division'].str.lower()
+    bdf_division['Code_division'] = bdf_division['Code_division'].str.replace('\*', '', regex=True)
+    table_bdf.drop(table_bdf[table_bdf['Division'] == True].index, inplace=True)
+
+    # Get groups
+    table_bdf['Groupe'] = table_bdf['Code sur\n2, 3, 4 positions'].str.endswith('**')
+    bdf_groupe = table_bdf.loc[table_bdf['Groupe'] == True, ['Code sur\n2, 3, 4 positions', 'Rubriques']]
+    bdf_groupe.rename(columns={'Code sur\n2, 3, 4 positions': 'Code_groupe', 'Rubriques': 'Label_groupe'}, inplace=True)
+    bdf_groupe['Label_groupe'] = bdf_groupe['Label_groupe'].str.lower()
+    bdf_groupe['Code_groupe'] = bdf_groupe['Code_groupe'].str.replace('\*', '', regex=True)
+    table_bdf.drop(table_bdf[table_bdf['Groupe'] == True].index, inplace=True)
+
+    # Get classes
+    table_bdf.loc[:, 'Classe'] = table_bdf.loc[:, 'Code sur\n2, 3, 4 positions'].str.endswith('*')
+    bdf_classe = table_bdf.loc[table_bdf['Classe'] == True, ['Code sur\n2, 3, 4 positions', 'Rubriques']]
+    bdf_classe.rename(columns={'Code sur\n2, 3, 4 positions': 'Code_classe', 'Rubriques': 'Label_classe'}, inplace=True)
+    bdf_classe.loc[:, 'Label_classe'] = bdf_classe.loc[:, 'Label_classe'].str.lower()
+    bdf_classe.loc[:, 'Code_classe'] = bdf_classe.loc[:, 'Code_classe'].str.replace('\*', '', regex=True)
+
+    table_bdf.drop(table_bdf[table_bdf['Classe'] == True].index, inplace=True)
+    table_bdf.drop(columns=['Code sur\n2, 3, 4 positions', 'Division', 'Groupe', 'Classe'], axis= 1, inplace=True)
+    table_bdf.drop(table_bdf[table_bdf['Code sur 5 positions'].isna()].index, axis = 0, inplace = True)
+
+    table_bdf.loc[:, 'Code_sous_classe'] = table_bdf.loc[:, 'Code sur 5 positions'].astype(int).astype(str).str.zfill(5)
+
+    # Extract first two digits for the division, the first three for the group, the first four for the class
+    table_bdf.loc[:, 'Code_division'] = table_bdf.loc[:, 'Code_sous_classe'].str[:2]  
+    table_bdf.loc[:, 'Code_groupe'] = table_bdf.loc[:, 'Code_sous_classe'].str[:3]
+    table_bdf.loc[:, 'Code_classe'] = table_bdf.loc[:, 'Code_sous_classe'].str[:4]
+
+    table_bdf.drop(columns=['Code sur 5 positions'], axis= 1, inplace=True)
+
+    # Merge with the tables of divisions, groups and classes extracted above
+    table_bdf = table_bdf.merge(bdf_division, on='Code_division', how='left')
+    table_bdf = table_bdf.merge(bdf_groupe, on='Code_groupe', how='left')
+    table_bdf = table_bdf.merge(bdf_classe, on='Code_classe', how='left')
+
+    table_bdf.rename(columns={'Rubriques': 'Label_sous_classe'}, inplace=True) 
+    bdf_nomenclature = table_bdf.loc[:, ['Label_division', 'Label_groupe', 'Label_classe', 'Label_sous_classe', 'Code_sous_classe']]
+    bdf_nomenclature.loc[:, 'code_coicop'] = bdf_nomenclature.loc[:, 'Code_sous_classe'].apply(lambda x: ".".join([x[:2], x[2:3], x[3:4], x[4:]]))
+
+    return bdf_nomenclature
+
+
+# A dictionnary used to adjust some BDF codes to match the CN nomenclature
+adjust_to_cn_nomenclature = {
+    '01.2.2.3': '01.2.1.1',     # Jus de fruits, sirops, boissons aromatisées
+    '01.2.2.4': '01.2.1.2',     # Jus de légumes
+    '01.2.1.1': '01.2.2.1',     # Café
+    '01.2.1.2': '01.2.3.1',     # Thé et plantes à infusion
+    '01.2.2.1': '01.2.5.1',     # Eaux minérales
+    '01.2.2.2': '01.2.6.1',     # Boissons gazeuses
+    '01.2.1.3': '01.2.9.1',     # Cacao et chocolat en poudre
+    '02.1.2.2': '02.1.9.1',     # Autres apéritifs à base de vin, champagne et autres mousseux
+    '02.2.1.1': '02.3.1.1',     # Cigarettes
+    '02.2.1.2': '02.3.1.2',     # Cigares et cigarillos
+    '02.2.1.3': '02.3.1.3',     # Tabac sous d’autres formes et produits connexes
+    '02.3.1.1': '02.4.1.1',     # Stupéfiants
+    '02.4.1.1': '02.5.1.1',     # Dépenses de boissons alcoolisées, tabac et stupéfiants : cadeau offert à un autre ménage
+    '04.4.3.1': '04.4.1.1',     # Factures d’eau résidence principale, autre logement, dépendance, terrain
+    '04.4.1.1': '04.4.2.1',     # Enlèvement des ordures : Redevance ordures ménagères
+    '04.4.2.1': '04.4.3.1',     # Assainissement
+    '04.4.4.1': '04.4.4.1',     # Charges collectives relatives au logement (payées isolément du loyer ou crédits
+    '05.5.1.3': '05.5.3.1',     # Réparation du gros outillage
+    '05.5.2.3': '05.5.3.2',     # Réparation des petits outillages
+    '08.1.1.1': '07.4.1.1',     # Services postaux
+    '07.3.6.1': '07.4.9.1',     # Autres services de transports
+    '07.4.1.1': '07.5.1.1',     # Autres dépenses de transport : cérémonie, séjours hors domicile, personnes vivant hors du domicile au moins un jour par semaine
+    '07.4.1.2': '07.5.1.2',     # Autres dépenses de transport : cadeau offert (à destination d’un autre ménage)
+    '08.1.3.1': '08.3.1.1',     # Services de téléphone, internet, recharges téléphoniques
+    '08.1.4.1': '08.4.1.1',     # Autres dépenses de communications : cadeau offert (à destination d'un autre ménage)
+    '09.1.1.1': '09.1.2.1',     # Appareils de réception, d’enregistrement et de reproduction du son
+    '09.1.1.2': '09.1.2.2',     # Téléviseurs, home cinéma, lecteur DVD de salon et portables
+    '09.1.2.1': '09.1.1.1',     # Equipement photographique et cinématographique
+    '09.1.2.2': '09.1.1.2',     # Instruments d'optique
+    '09.1.3.1': '08.1.1.1',     # Micro-ordinateurs, matériels et accessoires informatiques, consommables
+    '09.1.4.1': '09.5.2.1',     # Supports vierges ou enregistrés pour l’image et le son
+    '09.1.5.1': '09.4.1.1',     # Réparation des équipements et accessoires audiovisuels, photographiques et informatiques
+    '09.2.1.1': '09.2.2.1',     # Gros équipements pour les loisirs de plein air et les sports
+    '09.2.2.1': '09.5.1.1',     # Instruments de musique et accessoires
+    '09.2.2.2': '09.1.2.3',     # Gros équipements pour les loisirs d’intérieur
+    '09.2.3.1': '09.4.2.1',     # Réparation et entretien de biens durables pour les loisirs, les sports et la culture
+    '09.3.1.1': '09.2.1.1',     # Jeux, jouets et passe temps
+    '09.3.1.2': '09.2.2.2',     # Equipements de sport, de camping et de loisirs en plein air (pêche, chasse, ustensiles et vêtements spéciaux, matériel de camping)
+    '09.3.2.1': '09.3.1.1',     # Horticulture, accessoires et frais de livraison
+    '09.3.3.1': '09.3.2.1',     # Animaux d’agrément
+    '09.3.4.1': '09.3.2.2',     # Aliments autres animaux
+    '09.4.1.1': '09.4.2.2',     # Services sportifs et récréatifs (spectacles sportifs, participation loisirs, location matériel, cours et cotisations de loisirs, abonnement jeu vidéo... )
+    '09.4.2.1': '09.6.1.1',     # Cinémas, théâtres, salles de concert
+    '09.4.2.2': '09.6.1.2',     # Musées, jardins zoologiques et similaires
+    '09.4.2.3': '09.6.9.1',     # Services de télévision et de radiodiffusion (location, redevance, abonnement)
+    '09.4.2.4': '09.6.3.1',     # Smartbox et autres services de loisirs (animateurs, photographes, services pour animaux)
+    '09.4.3.1': '09.4.7.1',     # Jeux de hasard (loto, tiercé ...)
+    '09.5.1.1': '09.7.1.1',     # Livres
+    '09.5.2.1': '09.7.2.1',     # Journaux et périodiques
+    '09.5.3.1': '09.7.3.1',     # Imprimés divers
+    '09.5.4.1': '09.7.4.1',     # Articles de papéterie et de dessin
+    '09.6.1.1': '09.8.1.1',     # Voyages à forfait, week-end, excursions... yc voyage scolaire
+    '09.7.1.1': '09.9.1.1',     # Autres dépenses des loisirs : séjours hors domicile, personnes vivant hors du domicile au moins un jour par semaine
+    '09.7.1.2': '09.9.1.2',     # Autres dépenses de loisirs et culture : cadeau offert (à destination d’un autre ménage)
+    '10.1.2.1': '10.2.1.1',     # Enseignement secondaire
+    '10.1.3.1': '10.4.1.1',     # Enseignement supérieur
+    '10.1.4.1': '10.5.1.1',     # Enseignement ne correspondant à aucun niveau particulier
+    '10.1.5.1': '10.6.1.1',     # Autres dépenses d’enseignement : personnes vivant hors du domicile au moins un jour par semaine
+    '10.1.5.2': '10.6.1.2',     # Autres dépenses d’enseignement : cadeau offert (à destination d’un autre ménage)
+    '12.1.1.1': '13.1.3.1',     # Salons de coiffure et esthétique corporelle (yc cures thermales, tatouages, piercings)
+    '12.1.3.1': '13.1.3.2',     # Services des soins personnels (services des prostituées)
+    '12.1.2.1': '13.1.1.1',     # Appareils électriques pour les soins personnels
+    '12.1.2.2': '13.1.2.1',     # Autres articles et produits pour les soins personnels
+    '12.3.1.1': '13.2.1.1',     # Articles de bijouterie, de joaillerie et d’horlogerie
+    '12.3.2.1': '13.2.9.1',     # Articles de voyage et autres contenants d'effets personnels
+    '12.3.2.2': '13.2.9.2',     # Autres effets personnels
+    '12.5.1.1': '12.1.1.1',     # Assurances vie, décès
+    '12.5.2.1': '12.1.3.1',     # Assurances liées au logement
+    '12.5.3.1': '12.1.2.1',     # Assurances liées à la santé
+    '12.5.4.1': '12.1.4.1',     # Assurances liées aux transports
+    '12.5.5.1': '12.1.9.1',     # Autres assurances
+    '12.6.1.1': '12.2.1.1',     # Services financiers
+    '12.4.1.1': '13.3.1.1',     # Services de protection sociale (assistante maternelle, crèche, maison de retraite)
+    '12.7.1.1': '13.9.1.1',     # Autres servies
+    '12.7.1.2': '13.9.1.2',     # Caution pour la location d'un logement
+    '13.1.1.1': '17.1.1.1',     # Impôts et taxes de la résidence principale
+    '13.1.2.1': '17.1.2.1',     # Impôts et taxes pour une résidence secondaire ou un autre logement
+    '13.1.4.1': '17.1.3.1',     # Impôts sur le revenu
+    '13.1.5.1': '17.1.5.1',     # Taxes automobiles
+    '13.1.6.1': '17.1.6.1',     # Autres impôts et taxes
+    '13.2.1.1': '17.2.1.1',     # Remboursements de prêts pour la résidence principale
+    '13.2.2.1': '17.2.2.1',     # Remboursements des autres prêts immobiliers
+    '13.3.1.1': '17.3.1.1',     # Aides et dons en argent offerts par le ménage et pensions
+    '13.4.1.1': '17.4.1.1',     # Gros travaux pour la résidence principale
+    '13.4.2.1': '17.4.2.1',     # Gros travaux pour une résidence secondaire ou un autre logement
+    '13.5.1.1': '17.5.1.1',     # Remboursements de crédits à la consommation
+    '13.6.1.1': '17.6.1.1',     # Prélèvements de l'employeur
+    '13.7.1.1': '17.7.1.1',     # Achats de logements, garages, parkings, box et terrains
+    '13.7.2.2': '17.7.2.2',     # Epargne salariale
+    '14.1.1.1': '18.1.1.1',     # Allocatons logement reçues par le ménage
+    }
+
+
+def build_complete_bdf_nomenclature(year = 2017, to_csv=True):
+    """Build the complete BDF nomenclature DataFrame from the Excel file of BDF 2017 nomenclature and the dict to adjust to the national accounts.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the complete BDF nomenclature with
+                      columns for division, group, class, and subclass labels
+                      intial BDF codes and adjusted ones.
+    """
+    bdf_nomenclature = build_raw_bdf_nomenclature(year = 2017)
+    bdf_to_cn_dataframe = pd.DataFrame(list(adjust_to_cn_nomenclature.items()), columns = ['code_coicop', 'adjusted_bdf'])
+    bdf_nomenclature = bdf_nomenclature.merge(bdf_to_cn_dataframe, on = 'code_coicop', how ='outer')
+    bdf_nomenclature.loc[:, 'adjusted_bdf'] = bdf_nomenclature.loc[:, 'adjusted_bdf'].fillna(bdf_nomenclature.loc[:, 'code_coicop'])
+
+    # Optionally save to CSV
+    if to_csv:
+        output_path = os.path.join(
+            assets_directory,
+            'legislation',
+            f'bdf_{year}_nomenclature.csv'
+            )
+        bdf_nomenclature.to_csv(output_path, index=False)
+
+    return bdf_nomenclature
+
+
+if __name__ == '__main__':
+    bdf_nomenclature = build_complete_bdf_nomenclature(year = 2017, to_csv = True)
+    print(bdf_nomenclature.dtypes)
