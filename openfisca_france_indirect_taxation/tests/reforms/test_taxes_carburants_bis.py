7e1554d079016b585817dde8fe618a6e85ece68f
Benoît Courty <6603048+benoit-cty@users.noreply.github.com>
2026-01-29 18:02:01 +0100

    Update openfisca_france_indirect_taxation/tests/reforms/test_taxes_carburants_bis.py
    Co-authored-by: Hervé Darricau <62252565+Hervedarr31@users.noreply.github.com>

--- openfisca_france_indirect_taxation/tests/reforms/test_taxes_carburants_bis.py
+++ openfisca_france_indirect_taxation/tests/reforms/test_taxes_carburants_bis.py
@@ -9,7 +9,7 @@ from openfisca_france_indirect_taxation.reforms.rattrapage_diesel import reforme
 
 def test_rattrapage_diesel_bis():
     year = 2011
-    inflators_by_year = new_get_inflators_by_year(rebuild = False)
+    inflators_by_year = get_inflators_by_year(rebuild = False)
     inflation_kwargs = dict(inflator_by_variable = inflators_by_year[year])  # noqa analysis:ignore
     elasticities = get_elasticities(year)
     survey_scenario = SurveyScenario.create(
