d8c1e707e339431bf3fed49f286605e18c037604
Hervédarr31 <herve.darricau@ensae.fr>
2026-03-10 10:12:19 +0100

    Little change for code_region and notebook to test simulations

--- openfisca_france_indirect_taxation/variables/caracteristiques_menages/autres_caracteristiques.py
+++ openfisca_france_indirect_taxation/variables/caracteristiques_menages/autres_caracteristiques.py
@@ -52,11 +52,10 @@ class cataeu(YearlyVariable):
     label = 'catégorie de la commune de résidence 2011'
 
 
-class code_region(Variable):
+class code_region(YearlyVariable):
     value_type = str
     entity = Menage
     label = 'code region du menage'
-    definition_period = YEAR
     default_value = "99"
 
 
